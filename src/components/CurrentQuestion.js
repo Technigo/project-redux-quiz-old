9ebd9814f161fdf8d6e7fe278b861adce6d58d0f
Max Forssén <max.forssen@mentimeter.com>
2020-11-17 14:51:40 +0100

    added button

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -3,19 +3,15 @@ import { useSelector } from 'react-redux'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
-
   return (
     <div>
       <h1>{question.questionText}</h1>
-
-      
-      {/* <button onClick={() => dispatch(quiz.actions.goToNextQuestion())} type="button">
-        Next Question
-      </button> */}
+      {question.options.map((answer, index) => {
+        return <button type="button" key={index}>{answer}</button>
+      })}
     </div>
   )
 }
