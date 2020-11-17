e66ec63744f4b1e72ac93a2afdee58390987b207
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-17 14:01:05 +0100

    original set up for the project

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+
+//This is the main component
+//Next button will be included here
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const question = useSelector(
+    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+  );
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
     </div>
-  )
-}
+  );
+};
