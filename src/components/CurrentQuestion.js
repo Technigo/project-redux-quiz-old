d4e5d2c42b21717c1e4a8074375ae17cda75f532
Joel Harde <joelharde@Joels-MBP.localdomain>
2020-11-17 15:52:38 +0100

    Added buttons and options

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { quiz } from "reducers/quiz";
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const dispatch = useDispatch();
+  const question = useSelector(
+    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+  );
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+      {question.options.map((option, index) => {
+        return (
+          <button
+            className="option-button"
+            onClick={() =>
+              dispatch(
+                quiz.actions.submitAnswer({
+                  questionId: question.id,
+                  answerIndex: index,
+                })
+              )
+            }
+            key={option}
+          >
+            {option}
+          </button>
+        );
+      })}
+      <button onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
+        Next
+      </button>
     </div>
-  )
-}
+  );
+};
