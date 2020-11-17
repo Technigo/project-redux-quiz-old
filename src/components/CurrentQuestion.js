4ea5a62474845dbd6c59d9f37f7d721e2cb4537b
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-17 15:36:23 +0100

    Made going through questions possible

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,25 +1,26 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Status } from "../components/Status";
-
-//This is the main component
-//Next button will be included here
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Status } from 'components/Status'
+import { quiz } from 'reducers/quiz'
+// This is the main component
+// Next button will be included here
 
 export const CurrentQuestion = () => {
-  const question = useSelector(
-    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
-  );
+  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
 
-  if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>;
+  const dispatch = useDispatch()
+  const handleClick = () => {
+    dispatch(quiz.actions.goToNextQuestion())
   }
 
+  if (!question) {
+    return <h1>Oh no! I could not find the current question!</h1>
+  }
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
-
+      <button type="button" onClick={handleClick}>Next</button>
       <Status />
-
     </div>
-  );
-};
+  )
+}
