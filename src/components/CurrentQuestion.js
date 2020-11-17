7394a477ee1d82d7830266a3dd7424a69995f54a
Hanna Skeppe <hannaskeppe@gmail.com>
2020-11-17 14:29:24 +0100

    Added disbled functionanlity to button etc.

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -8,7 +8,8 @@ export const CurrentQuestion = () => {
     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
   );
   const quizOver = useSelector((state) => state.quiz.quizOver);
-
+  const answer = useSelector((state) => state.quiz.answers.find((a) => (a.questionId === question.id)))
+  console.log(answer)
   // const questions = useSelector((state) => state.quiz.questions);
   // const answers = useSelector((state) => state.quiz.answers);
 
@@ -21,11 +22,17 @@ export const CurrentQuestion = () => {
       {!quizOver && (
         <div>
           <h1>Question: {question.questionText}</h1>
-          {question.options.map((option) => (
-            <button type="button">{option}</button>
+          {question.options.map((option, index) => (
+            <button
+              disabled={answer}
+              //className={!answer ? "default-options-button" ? answer}
+              type="button"
+              onClick={() => dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))}>{option}
+            </button>
           ))}
           <button
             type="button"
+            disabled={!answer}
             onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
             Next Question
           </button>
