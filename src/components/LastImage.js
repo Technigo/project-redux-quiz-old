fd4761831703461278ee57c993874933cd4304b0
Annika Melin <melin.anki>
2020-11-18 16:00:03 +0100

    changed last image alt to last

--- src/components/LastImage.js
+++ src/components/LastImage.js
@@ -3,9 +3,9 @@ import React from 'react'
 import friends from './images/friends.jpg'
 
 export const LastImage = () => {
-    return (
-        <>
-            <img src={friends} className="question-image summary-image" alt="last image" />
-        </>
-    )
+  return (
+    <>
+      <img src={friends} className="question-image summary-image" alt="last" />
+    </>
+  )
 }
