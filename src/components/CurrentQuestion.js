bec94b635007eb6b923cd0dfaf7a9406627b72b6
Sofia Sousa <sofiavazsousa@gmail.com>
2020-11-18 09:36:18 +0100

    Add function to display the correct answer

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,17 +1,40 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { quiz } from '../reducers/quiz'
 
 export const CurrentQuestion = () => {
+  const dispatch = useDispatch()
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  // const checkAnswer = useSelector((state) => {
+  //   const answer = state.quiz.answers.find((questionId) => question.id === questionId)
+  //   if (answer) {
+  //     return answer
+  //   } else {
+  //     return null
+  //   }
+  // })
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
 
+  const buttonOnClick = (option) => {
+    return dispatch(quiz.actions.submitAnswer(
+      { questionId: question.id, answerIndex: question.options.indexOf(option) }
+    ))
+  }
   return (
     <div>
       <h1>{question.questionText}</h1>
-      <div>{question.options.map((option) => <p>{option}</p>)}</div>
+      <div>
+        {question.options.map((option) => <button onClick={() => buttonOnClick(option)} type="button">{option}</button>)}
+      </div>
     </div>
   )
 }
+
+/* <button onClick={() => dispatch(quiz.actions.goToNextQuestion())} type="button">
+        Next Question
+      </button> */
+
