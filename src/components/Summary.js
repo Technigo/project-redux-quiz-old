250658d9c31d134fcd5f3a7305198b46b04342e7
Mir Albertioni <fethullah.dincer@gmail.com>
2021-04-16 14:34:16 +0200

    done

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,23 +3,37 @@ import { useSelector, useDispatch } from 'react-redux' // useDispatch
 import {quiz} from "../reducers/quiz"
 
 
-const Summary = ()=>{
+const Summary = ({setSummary})=>{
+  const dispatch = useDispatch();
     const answers = useSelector((state) => state.quiz.answers)
-
-console.log(answers)
+    
+    const questions = useSelector((state) => state.quiz.questions)
+    const correct = questions[0].options[questions[0].correctAnswerIndex]
 
   return(
-    <>
-    <h1>This is a test</h1>
-    {answers.map((y) => (
+    <div className="summarySection">
+      <div className="container">
+    <h1>Look your results 🗒️</h1>
+    
+    <div ClasName="text-content"> 
+    {answers.map((answers) => (
         <>
-        <p>{y.answer} </p>
-        <p>{y.isCorrect}</p>
+        <p className="title">Question:</p><p>{answers.question.questionText}</p>
+        <p className="title" >You chose:</p><p>{answers.answer} {answers.isCorrect ? "✅" : "❌"}   </p>
+        <p>Correct Answer: {answers.question.options[answers.question.correctAnswerIndex]}</p>
         </>
     ))}
-    </>
+    <button onClick={()=> {
+      dispatch(quiz.actions.restart())
+      setSummary(false)
+      } }>Restart🔄</button>
+    </div>
+
+    </div>
+    </div>
 
   )
 }
-
 export default Summary
+
+
--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -2,8 +2,11 @@ import { createSlice } from '@reduxjs/toolkit'
 
 // Change these to your own questions!
 const questions = [
-  { id: 1, questionText: 'Who set the Olympic record for the 100m dash in 2012?', options: ['Usain Bolt', 'Justin Gatlin', 'Tyson Gay', 'Asafa Powell'], correctAnswerIndex: 0 },
-  { id: 2, questionText: 'When was Michael Phelps last named male World Swimmer of the Year?', options: ['2012', '2014', '2016', '2018'], correctAnswerIndex: 2 }
+  { id: 1, questionText: ' Which city is associate with tulips?', options: [ 'Dubai','Amsterdam','Paris','Caracas'], correctAnswerIndex: 1 },
+  { id: 2, questionText: 'What is the national emblem of Sweden?', options: ['Two-headed eagle','Three lions Cross', 'Three Crowns', 'Iron Cross'], correctAnswerIndex: 2 },
+  { id: 3, questionText: 'What country has the largest Muslim population?', options: ['Libya', 'Indonesia','Iraq','Nigeria'], correctAnswerIndex: 1 },
+  { id: 4, questionText: 'What European country is divided into departments?', options: ['Switzerland', 'France', 'Germany', 'Sweden'], correctAnswerIndex: 1 },
+  { id: 5, questionText: 'What is the name of the long, narrow country on South America’s Pacific coast?', options: ['Chile', 'Brazil', 'Colombia', 'Argentina'], correctAnswerIndex: 0 }
 ]
 
 const initialState = {
@@ -16,26 +19,10 @@ const initialState = {
 export const quiz = createSlice({ 
   name: 'quiz',
   initialState,
-  reducers: { // This is our Reducer
-
-    /**
-     * Use this action when a user selects an answer to the question.
-     * The answer will be stored in the `quiz.answers` state with the
-     * following values: quiz.answer{questionID, answerIndex, question}
-     *
-     *    questionId  - The id of the question being answered.
-     *    answerIndex - The index of the selected answer from the question's options.
-     *    question    - A copy of the entire question object, to make it easier to show
-     *                  details about the question in your UI.
-     *    answer      - The answer string.
-     *    isCorrect   - true/false if the answer was the one which the question says is correct.
-     *
-     * When dispatching this action, you should pass an object as the payload with `questionId`
-     * and `answerIndex` keys. See the readme for more details.
-     */
+  reducers: { 
     submitAnswer: (state, action) => {
       const { questionId, answerIndex } = action.payload
-      const question = state.questions.find((q) => q.id === questionId)
+    const question = state.questions.find((q) => q.id === questionId)
       console.log()
       if (!question) {
         throw new Error('Could not find question! Check to make sure you are passing the question id correctly.')
@@ -54,14 +41,7 @@ export const quiz = createSlice({
       })
     },
 
-    /**
-     * Use this action to progress the quiz to the next question. If there's
-     * no more questions (the user was on the final question), set `quizOver`
-     * to `true`.
-     *
-     * This action does not require a payload.
-     */
-    goToNextQuestion: (state) => { 
+      goToNextQuestion: (state) => { 
       if (state.currentQuestionIndex + 1 === state.questions.length) {
         state.quizOver = true // if his is the last question of possible questions then our quizz is over
       } else {
@@ -69,13 +49,6 @@ export const quiz = createSlice({
       }
     },
 
-    /**
-     * Use this action to reset the state to the initial state the page had
-     * when it was loaded. Who doesn't like re-doing a quiz when you know the
-     * answers?!
-     *
-     * This action does not require a payload.
-     */
     restart: () => {
       return initialState
     }
