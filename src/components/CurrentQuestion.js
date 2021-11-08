017a088cc1270d1fa775d00ab6f8917c74665c53
Linnea <linnea.iseb@gmail.com>
2021-11-08 15:01:30 +0100

    Added the options to the question component

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { quiz } from '../reducers/quiz';
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex]);
+
+  const dispatch = useDispatch();
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
+
+  const onAnswerSubmit = (id, index) => {
+    dispatch(quiz.actions.submitAnswer({ questionId: id, answerId: index }));
+  };
 
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+      {question.options.map((item, index) => (
+        <button type="submit" key={item} onClick={() => onAnswerSubmit(question.id, index)}>
+          {item}
+        </button>
+      ))}
     </div>
-  )
-}
+  );
+};
