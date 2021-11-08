bbd5285b55caff551e7cec94f6aa0438c84ffb7a
Sarah Mottram <smelbows@gmail.com>
2021-11-08 15:33:25 +0100

    added options map and button on click action

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from "../reducers/quiz"
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const dispatch = useDispatch()
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
 
+  const onAnswerSubmit = (id, index) => {
+    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }))
+  }
+
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+      {question.options.map((option, index) => (
+      <button key={option.id} onClick={() => onAnswerSubmit(question.id, index)}>{option.answer}</button>
+      ))
+    }
     </div>
   )
 }
