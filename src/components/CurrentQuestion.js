5c112cc17d01d81c57419099c5d649824bd572c0
Sarah Mottram <smelbows@gmail.com>
2021-11-08 20:41:08 +0100

    added message for correct or incorrect answer

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,39 +1,32 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { quiz } from '../reducers/quiz'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { quiz } from "../reducers/quiz";
 
 export const CurrentQuestion = () => {
   const question = useSelector(
     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
-  )
-  const checkAnswer = useSelector(
-    (state) => state.quiz.answers[state.quiz.isCorrect]
-  )
-  const dispatch = useDispatch()
+  );
+  const usersAnswer = useSelector(
+    (state) => state.quiz.answers[state.quiz.currentQuestionIndex]
+  );
+  const dispatch = useDispatch();
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   const onAnswerSubmit = (id, index) => {
     dispatch(
       quiz.actions.submitAnswer({ questionId: id, answerIndex: index }),
-      index === question.correctAnswerIndex ? <p>Correct!</p> : <p>Incorrect</p>
-    )
-    console.log('correct answer:', question.correctAnswerIndex)
-    console.log('chosen answer:', index)
-  }
+      // index === question.correctAnswerIndex ? <p>Correct!</p> : <p>Incorrect</p>
+    );
+    console.log("correct answer:", question.correctAnswerIndex);
+    console.log("chosen answer:", index);
+  };
 
   const onNextQuestionSubmit = () => {
-    dispatch(quiz.actions.goToNextQuestion())
-  }
-
-  // {
-  //   checkAnswer && <p>Correct!</p>
-  // }
-  // {
-  //   !checkAnswer && <p>Incorrect!</p>
-  // }
+    dispatch(quiz.actions.goToNextQuestion());
+  };
 
   return (
     <div>
@@ -47,7 +40,10 @@ export const CurrentQuestion = () => {
         </button>
       ))}
       <p>{question.correctAnswerIndex}</p>
+      {usersAnswer?.isCorrect && <p>Correct!</p>}
+      {usersAnswer?.isCorrect === false && <p>Incorrect!</p>}
+
       <button onClick={() => onNextQuestionSubmit(question.id)}>Next</button>
     </div>
-  )
-}
+  );
+};
