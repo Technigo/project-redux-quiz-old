af18d43bc62e48c42d5ccd00467c333f7bae4b2c
Kaliine <80845418+Kaliine@users.noreply.github.com>
2021-11-09 22:21:48 +0100

    added a summary component

---
+++ src/components/Summary.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { ResetButton } from './Buttons';
+
+export const Summary = ({ setWasCorrectAnswerSelected }) => {
+    const answersArray = useSelector((state) => state.quiz.answers);
+    const rightAnswersArray = answersArray.filter(
+        (answer) => answer.isCorrect === true
+    );
+
+    return (
+        <SummaryOuterWrapper>
+            {answersArray.map((answer) => {
+
+                return (
+                    <SummaryInnerWrapper key={answer.questionId}>
+
+                        <SummaryHeader>
+                            Question: {answer.question.questionText}
+                        </SummaryHeader>
+
+                        <SummaryTextHighlighted>
+                            Your answer: {answer.answer}
+                        </SummaryTextHighlighted>
+
+                        <SummaryText>
+                            Correct answer:{' '}
+                            {answer.question.options[answer.question.correctAnswerIndex]}
+                        </SummaryText>
+                        
+                    </SummaryInnerWrapper>
+                );
+            })};
+
+            <p>Your score was: {rightAnswersArray.length} of {answersArray.length}</p>
+            
+            {<ResetButton
+                title="Reset Quiz"
+                setWasCorrectAnswerSelected={setWasCorrectAnswerSelected}
+            />}
+
+        </SummaryOuterWrapper>
+    );
+};
+
+            
+            
