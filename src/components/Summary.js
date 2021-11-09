b663e87f4a613fe8ab895d44984837b5846632ed
pdetli <74938586+pdetli@users.noreply.github.com>
2021-11-09 13:09:07 +0100

    testing to first commit

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { quiz } from '../reducers/quiz'
+import React from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { quiz } from "../reducers/quiz"
 
 const Summary = () => {
   const dispatch = useDispatch()
   const answers = useSelector((state) => state.quiz.answers)
+  console.log("hello")
 
-    
-return (
-    
- <div>
-    <h1>TEST</h1>
-    <button onClick={ () => dispatch(quiz.actions.restart())}>RESTART</button>
-    {answers.map((answer) => (
-  <div key={answer.question.questionText}>
-    <p >{answer.question.questionText}
-        <span>{answer.answer}</span>
-        {answer.isCorrect ? "✅" : "❌"}
-    </p>
-    {!answer.isCorrect ? answer.question.options[answer.question.correctAnswerIndex] : ''}
+  return (
+    <div>
+      <h1>TEST</h1>
+      <button onClick={() => dispatch(quiz.actions.restart())}>RESTART</button>
+      {answers.map((answer) => (
+        <div key={answer.question.questionText}>
+          <p>
+            {answer.question.questionText}
+            <span>{answer.answer}</span>
+            {answer.isCorrect ? "✅" : "❌"}
+          </p>
+          {!answer.isCorrect
+            ? answer.question.options[answer.question.correctAnswerIndex]
+            : ""}
+        </div>
+      ))}
     </div>
- ))}  
- </div>
-    
-)}
+  )
+}
 
 export default Summary
