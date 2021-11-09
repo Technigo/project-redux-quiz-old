6c8b08a1ce08f8a2ea294a258adcff5e354d4ad4
JohannaMJ <johanna.mannestal@gmail.com>
2021-11-09 10:52:12 +0100

    displaying summary result

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,26 +1,56 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { quiz } from '../reducers/quiz'
-import { useHistory } from 'react-router-dom'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { quiz } from '../reducers/quiz';
+import { useHistory } from 'react-router-dom';
 
 const Summary = () => {
-  const dispatch = useDispatch()
+  const answers = useSelector((state) => state.quiz.answers);
+  console.log(answers);
+  const dispatch = useDispatch();
 
-  const history = useHistory()
+  const history = useHistory();
 
   const handleRestart = () => {
-    dispatch(quiz.actions.restart())
-    history.push('/')
-  }
+    dispatch(quiz.actions.restart());
+    history.push('/');
+  };
+  const correctAnswers = answers.filter((answers) => answers.isCorrect);
+  const numberOfCorrectAnswers = correctAnswers.length;
+
+  const numberOfQuestions = answers.length;
 
   return (
     <div>
       <h1> Great work you managed to answer all the questions in our quiz! </h1>
+      {answers.map((item) => {
+        const questionNumber = item.question.id;
+        const questionText = item.question.questionText;
+        const usersAnswer = item.question.options[item.answerIndex].answer;
+        const correctAnswer =
+          item.question.options[item.question.correctAnswerIndex].answer;
+
+        return (
+          <div key={questionNumber}>
+            <p>
+              {questionNumber}. {questionText}
+            </p>
+            <p>Your answer was {usersAnswer}</p>
+            <p>
+              The correct answer is:
+              {correctAnswer}
+            </p>
+          </div>
+        );
+      })}
+      <p>
+        You got {numberOfCorrectAnswers} / {numberOfQuestions} correct answers
+      </p>
+
       <button onClick={handleRestart}>
         Would you like to give it a go again?
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Summary
+export default Summary;
--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { quiz } from 'reducers/quiz'
-import { CurrentQuestion } from 'components/CurrentQuestion'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { quiz } from 'reducers/quiz';
+import { CurrentQuestion } from 'components/CurrentQuestion';
 
 const Questions = () => {
-  return <CurrentQuestion />
-}
+  return <CurrentQuestion />;
+};
 
-export default Questions
+export default Questions;
