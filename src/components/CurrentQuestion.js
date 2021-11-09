c113843edd02fad2586336a4bf66849ab3bc3292
Jsfrulle <Jessicamhermansson@gmail.com>
2021-11-09 20:14:08 +0100

    just add black default(body) so summary is showing

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -15,16 +15,13 @@ export const CurrentQuestion = () => {
     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
   );
 
-
-
   /* Here we get the length of the arry(all the questions) */
- const questionsLength = useSelector( (state) => state.quiz.questions.length)
+ const questionsLength = useSelector((state) => state.quiz.questions.length)
 
  /* Here we pass the value from the "questionsLength" so we can decreas the value of questions left */
  const [questionsCount, setQuestionsCount] = useState(questionsLength);
 
  /* The progressbar for the question, using npm install --save @ramonak/react-progress-bar (read more here; https://www.npmjs.com/package/@ramonak/react-progress-bar#examples) */
-
  const [completed, setCompleted] = useState(20);
 
 
@@ -52,19 +49,14 @@ export const CurrentQuestion = () => {
     return <Summary />
   }
 
+
+
   const onSubmitAnswer = (id, index) => {
     // what exact function we need to call to uppdate the store
     dispatch(
       quiz.actions.submitAnswer({ questionId: id, answerIndex: index })
     );
 
-/* when clicking a btn the value of questionsCount(=Question left) decreses  */
-    setQuestionsCount(questionsCount - 1)
-
-
-   /* Update the progress-bar while clickin with 20% */ 
-
-    setCompleted(completed + 20)
 
     // first dispatch is to call submitAnswer, and another one is to call goToNextQuestion function
     // because we want to go to the next question directly
@@ -73,6 +65,13 @@ export const CurrentQuestion = () => {
     );
 
 
+/* when clicking a btn the value of questionsCount(=Question left) decreses  */
+setQuestionsCount(questionsCount - 1);
+
+
+/* Update the progress-bar while clickin with 20% */ 
+
+ setCompleted(completed + 20);
 
    
         
@@ -101,10 +100,13 @@ export const CurrentQuestion = () => {
               <button
                 className="button"
                 type="button"
-                key={item} /* pass function to the dispatch to update the state, exessing
+                key={item}
+                
+                
+/* pass function to the dispatch to update the state, exessing
 quiz object: quiz.actions.submitAnwer(), then we need to pass argument to our submit function
 answerId is a specific answer of a question */
-                onClick={() => onSubmitAnswer(question.id, index)}>
+                onClick={()=> onSubmitAnswer(question.id, index)}>
                 <span className="button-text">{item}</span>
               </button>
             ))}
