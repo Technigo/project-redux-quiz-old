fe60c25b8094906f6bac43abf00f4a1529a7caae
Elsa-Johanna <elsa.bjelkedal@gmail.com>
2021-11-10 16:29:07 +0100

    added a wanna play again- link on the summary page which redirects to the quiz

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { quiz } from "../reducers/quiz";
+import { Link } from "react-router-dom";
 
-export const Summary = () => {
-  return <div>Summary</div>;
+export const Summary = (CurrentQuestion) => {
+  return (
+    <div>
+      Summary
+      <Link to="/quiz">
+        <button onClick={CurrentQuestion}>Wanna play again?</button>
+      </Link>
+    </div>
+  );
 };
