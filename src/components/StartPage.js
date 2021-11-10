ae5f5b671e4bda92ad6a80cba8333bb799e85dbd
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-11-10 18:15:56 +0100

    Tried with background on startpage

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './StartPage.css';
+
 
 const StartPage = () => {
     return (
 
       <div>
-        {/* <img src="./xxxxxxxxxxx" alt="movie" /> */}
+        <img src="./assets/movie-quiz-background.jpg" alt="movie" />
         <h1>MOVIE TIME</h1>
         <Link to="/quiz">
           <button className="start-button">START QUIZ HERE!</button>
