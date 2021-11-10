5829710c65222f88563f40b2349d876156b5bdc0
Jenny Quach <quach.jen@gmail.com>
2021-11-10 10:39:55 +0100

    button component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { quiz } from 'reducers/quiz';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import StartPage from 'components/StartPage';
+import { Button } from 'components/Buttons';
+import { SubmitButton } from 'components/Buttons';
 
 import { CurrentQuestion } from 'components/CurrentQuestion';
 
