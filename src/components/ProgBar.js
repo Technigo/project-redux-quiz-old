fc0a9f29d1029eb36affc9be220b0edd4c3bb482
Ida Aspen <ida.aspen@outlook.com>
2021-11-10 09:37:10 +0100

    fixed ProgBar component

--- src/components/ProgBar.js
+++ src/components/ProgBar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import { useSelector } from 'react-redux';
+import './progBar.css';
 
 export const ProgBar = () => {
   const question = useSelector(
     (store) => store.quiz.questions[store.quiz.currentQuestionIndex]
   );
-  console.log('QUESTION', question);
+  // console.log('QUESTION', question);
 
-  return (
-    <div>
-      <ProgressBar now={question.id} label={`${(question.id / 5) * 100}%`} />
-    </div>
+  const now = (question.id / 5) * 100;
+  const progressInstance = (
+    <ProgressBar now={now} label={`${now}%`} visuallyHidden />
   );
+
+  return progressInstance;
 };
 
-// this is just a experimental progressbar I played with! it's not imported anywhere yet :)
+// followed this: https://react-bootstrap.github.io/components/progress/
