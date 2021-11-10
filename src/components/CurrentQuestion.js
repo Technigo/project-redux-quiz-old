f3fc27c3f4272ff9430f7467ef963e91b49998ca
Gustav Frid <gustav.frid@gmail.com>
2021-11-10 13:20:04 +0100

    fix button styling when click option + next question button + Show results button

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,32 +1,36 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-
-import { quiz } from "../reducers/quiz";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { quiz } from '../reducers/quiz'
 
 //Background-image: https://images.unsplash.com/photo-1608371945786-d47d3cdd31da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80
 
 export const CurrentQuestion = () => {
-  const dispatch = useDispatch();
-  const question = useSelector(
-    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
-  );
-  const answers = useSelector((state) => state.quiz.answers);
-  const currentQuestionIndex = useSelector(
-    (state) => state.quiz.currentQuestionIndex
-  );
-  const quizOver = useSelector((state) => state.quiz.quizOver);
-  console.log(answers);
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const questions = useSelector(state => state.quiz.questions)
+  const question = useSelector(state => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const answers = useSelector(state => state.quiz.answers)
+  const currentQuestionIndex = useSelector(state => state.quiz.currentQuestionIndex)
+  const quizOver = useSelector(state => state.quiz.quizOver)
+  // console.log(answers)
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>;
+    return <h1>Oh no! I could not find the current question!</h1>
   }
 
-  const onSubmitAnswer = (index) => {
-    dispatch(
-      quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index })
-    );
-    console.log("click");
-  };
+  const onSubmitAnswer = index => {
+    dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))
+    console.log('click')
+  }
+
+  const onButtonClick = () => {
+    if (answers.length !== questions.length) {
+      dispatch(quiz.actions.goToNextQuestion())
+    } else {
+      navigate('/summary')
+    }
+  }
 
   // efter ett val så får man reda på om det var rätt/fel samt att knapparna blir disabled + det kommer en "next question" knapp
 
@@ -36,35 +40,27 @@ export const CurrentQuestion = () => {
       {question.options.map((option, index) => (
         <button
           key={option}
+          type='button'
           disabled={answers.length === question.id}
           className={
-            answers[currentQuestionIndex]?.isCorrect &&
             answers[currentQuestionIndex]?.answerIndex === index
-              ? "button correct"
-              : answers.length !== question.id
-              ? "button"
-              : "button incorrect"
+              ? answers[currentQuestionIndex]?.isCorrect
+                ? 'button correct'
+                : 'button incorrect'
+              : 'button'
           }
-          onClick={() => onSubmitAnswer(index)}
-        >
+          onClick={() => onSubmitAnswer(index)}>
           {option}
         </button>
       ))}
       {answers.length === question.id && !quizOver && (
-        <button onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
-          {answers.length + 1 === question.length
-            ? "Show results"
-            : "Next Question"}
+        <button type='button' onClick={() => onButtonClick()}>
+          {answers.length !== questions.length ? 'Next Question' : 'Show results'}
         </button>
       )}
-      {/* {quizOver && (
-        <button onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
-          Show results
-        </button>
-      )} */}
     </div>
-  );
-};
+  )
+}
 // button:focus{background-color:red;}
 // export const Square = ({ value, index }) => {
 //   const dispatch = useDispatch();
