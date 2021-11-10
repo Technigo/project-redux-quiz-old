8d52faa219a34a0856e7900dc4f52305c4495f49
Jessica Nygren <jessicanygren@C02D54BJMD6R.local>
2021-11-10 22:15:31 +0100

    summary page

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,16 +1,28 @@
 import React from 'react'
 // import { quiz } from '../reducers/quiz'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 export const Summary = () => {
   const storeAnswer = useSelector((state) => state.quiz.answers)
-  console.log(storeAnswer)
+  console.log('summary:', storeAnswer)
+
+  const storeQuestion = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  console.log('question;', storeQuestion)
+
+  const dispatch = useDispatch() ////användas för restart btn
 
   return (
-    <div>
-      <p className='summary-text'>SUMMARY!</p>
-      <p className='summary-text'>Question 1</p>
-      <p className='summary-text'>{storeAnswer[0].answer}</p>
+    <>
+    {storeAnswer.map((results) => (
+    <div key={results.questionId} style={{ backgroundColor: storeQuestion.backgroundColor }}>
+          <h3 className='question-title'>{results.questionId}</h3>
+          <div className='text'>
+            <p>{results.question.questionText}</p>
+            <p className='your-answer'>Your answer {results.answer}</p>
+          </div>
+        { results.isCorrect ? <i className="fas fa-check"/>: <i className="fas fa-times"/>}
     </div>
+      ))}
+    </>
   )
 }
--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -30,7 +30,7 @@ const questions = [
     correctAnswerIndex: 1,
     backgroundColor: 'pink',
   },
-  {
+/*   {
     id: 4,
     questionText: 'What are you afraid of if you are Syngenesophobic?',
     options: ['Syringes', 'Genetically modified crops', 'Relatives', 'Snakes'],
@@ -44,8 +44,7 @@ const questions = [
     options: ['Stone', 'Ice', 'Cow manure', 'Wood'],
     correctAnswerIndex: 2,
     backgroundColor: 'orange',
-    // quizOver: 'true',
-  },
+  }, */
 ]
 
 const initialState = {
