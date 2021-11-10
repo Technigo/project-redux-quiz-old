40689240bcf76c85bd230ec032af7420196af5d3
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-11-10 10:41:39 +0100

    added colors for right wrong answer

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,43 +1,29 @@
-import React, { useState, useEffect } from "react"
-
-// import { useSelector, useDispatch } from "react-redux"
+import React from "react";
 
 export const Button = ({
-  questionId,
-  correctAnswerIndex,
-  onClick,
   disabled,
   item,
-  answerIndex,
+  answer,
+  index,
+  question,
+  onClick,
 }) => {
-  //   const onAnswerSubmit = (id, index) => {
-  //     dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }))
-  //   }
-  const [color, setColor] = useState("grey")
-
-  useEffect(() => {
-    setColor("grey")
-  }, [questionId])
-
-  const onAnswerClick = () => {
-    onClick()
-    if (answerIndex === correctAnswerIndex) {
-      setColor("green")
-    } else {
-      setColor("red")
-    }
-  }
-
   return (
     <div>
       <button
         disabled={disabled}
-        onClick={onAnswerClick}
-        style={{ backgroundColor: `${color}` }}
         type="button"
+        className={
+          !answer
+            ? "default-button"
+            : index === question.correctAnswerIndex
+            ? "right-button"
+            : "wrong-button"
+        }
+        onClick={onClick}
       >
         {item}
       </button>
     </div>
-  )
-}
+  );
+};
--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,50 +1,53 @@
-import React from "react"
-import { useSelector, useDispatch } from "react-redux"
-import Summary from "components/Summary"
-import { Button } from "components/Button"
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Summary from "components/Summary";
+import { Button } from "components/Button";
 
-import { quiz } from "../reducers/quiz"
+import { quiz } from "../reducers/quiz";
 
 export const CurrentQuestion = () => {
-  // const currentQuestion = useSelector((store) => store.quiz.currentQuestionIndex)
   const question = useSelector(
     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
-  )
+  );
   const answer = useSelector((state) =>
     state.quiz.answers.find((answer) => answer.questionId === question.id)
-  )
-  const dispatch = useDispatch()
-  const quizOver = useSelector((state) => state.quiz.quizOver)
+  );
+  const dispatch = useDispatch();
+  const quizOver = useSelector((state) => state.quiz.quizOver);
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   const onAnswerSubmit = (id, index) => {
-    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }))
-  }
+    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }));
+  };
 
   const handleClick = () => {
-    dispatch(quiz.actions.goToNextQuestion({}))
-  }
+    dispatch(quiz.actions.goToNextQuestion({}));
+  };
 
   if (!quizOver)
     return (
       <div>
         <h1>Question: {question.questionText}</h1>
         {question.options.map((item, index) => (
-          <Button
-            questionId={question.id}
-            correctAnswerIndex={question.correctAnswerIndex}
-            key={index}
-            answerIndex={question.answerIndex}
-            disabled={!!answer}
-            onClick={() => onAnswerSubmit(question.id, index)}
-            type="button"
-            item={item}
-          >
-            {" "}
-          </Button>
+          <div key={index}>
+            <Button
+              questionId={question.id}
+              correctAnswerIndex={question.correctAnswerIndex}
+              index={index}
+              answerIndex={question.answerIndex}
+              disabled={!!answer}
+              onClick={() => onAnswerSubmit(question.id, index)}
+              type="button"
+              item={item}
+              question={question}
+              answer={answer}
+            >
+              {" "}
+            </Button>
+          </div>
         ))}
         {/* {question.options.map((item, index) => (
           <button
@@ -60,8 +63,8 @@ export const CurrentQuestion = () => {
         </button>
         <p>{question.id}/5</p>
       </div>
-    )
-  return <Summary />
-}
+    );
+  return <Summary />;
+};
 
 // not submitting answer to store on "answer button" but save it and submit with Next question
