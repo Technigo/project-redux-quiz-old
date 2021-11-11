d141fe1a1ea133fbbf54ac1fba5ba66f77bf2a50
AschwinSiaila <aschwin76@hotmail.com>
2021-11-11 09:56:24 +0100

    changing answerbutton

--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { CurrentQuestion } from "./CurrentQuestion";
+
+const isCorrect = useSelector((state) => state.quiz.answers[state.quiz.currentQuestionIndex].isCorrect);
 
 const AnswerButton = (props) => {
   return (
-    <button
-      className="button"
-      type="button"
-      key={props.key}
-      onClick={props.onClick}>
-      <span className="button-text">
-        {props.text}
-      </span>
+    <button className={!isCorrect ? "button" : "correctbutton"} type="button" key={props.key} onClick={props.onClick}>
+      <span className="button-text">{props.text}</span>
     </button>
-  )
-}
+  );
+};
 
-export default AnswerButton
+export default AnswerButton;
