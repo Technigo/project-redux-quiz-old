a345760140abcf4bb2c288bdd6ce6cc109b1be71
MT-dotse <madelene.trang333@gmail.com>
2021-11-11 16:48:50 +0200

    added styling component

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -5,19 +5,32 @@ import { Summary } from './Summary';
 import { quiz } from '../reducers/quiz';
 
 const Main = styled.div`
-	width: 90%;
+	width: 100%;
+	height: 100vh;
 	margin: 0 auto;
+	background-image: url(./assets/black-night.jpg);
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: left;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 `; // This will style everything in this component
 const Questiontext = styled.h1`
-	font-family: 'Euphoria Script', cursive;
+	font-family: 'Calligraffitti', cursive;
 	font-size: 50px;
 	text-align: centre;
+	color: white;
+	margin: 0 20px 30px;
+	text-shadow: 2px #ffe227;
+	text-shadow: 0 0 4px #d4af37, 0 0 6px #800000;
 `; //This styles the H1 question
 const Buttonssection = styled.section`
 	justify-items: centre;
-`; // Styles the button container
+`; // Styles the button container, add display: inline for desktop
 const Choice = styled.div`
-	//display: inline;
+	justify-content: centre;
 `; // styles the choice buttons
 const Choicebutton = styled.button`
 	width: 75%;
@@ -29,13 +42,19 @@ const Nextbutton = styled.button`
 	width: 30%;
 	margin: 15px;
 	padding: 15px;
+	border-radius: 20px;
 `; // Styles the next button
 const Chosenanswer = styled.span`
 	color: white;
 	font-size: 25px;
 	font-family: 'Redressed', cursive;
 `; // Styles the text on choice button
-const Feedback = styled.div``;
+const Feedback = styled.div`
+	color: white;
+`;
+const Questioncontainer = styled.div`
+	margin: 0 auto;
+`;
 
 export const CurrentQuestion = () => {
 	const question = useSelector(
@@ -65,53 +84,55 @@ export const CurrentQuestion = () => {
 
 	return (
 		<Main>
-			{/*{answers[currentQuestionIndex] ? answers[currentQuestionIndex].isCorrect : 'no answer'}*/}
-			<Questiontext>Question: {question.questionText}</Questiontext>
-			<Buttonssection>
-				{question.options.map((item, index) => (
-					<Choice>
-						<Choicebutton
-							key={item}
-							onClick={() => onAnswerSubmit(question.id, index)}
-							style={{
-								backgroundColor: !answers
-									? '#EADCA6'
-									: index === question.correctAnswerIndex
-									? '#17D7A0'
-									: '#B91646',
-							}}
-						>
-							<Chosenanswer>{item}</Chosenanswer>{' '}
-						</Choicebutton>
-					</Choice>
-				))}
-				<Nextbutton
-					disabled={!answers[currentQuestionIndex]}
-					onClick={() => dispatch(quiz.actions.goToNextQuestion())}
-				>
-					{' '}
-					Next Question
-				</Nextbutton>
-			</Buttonssection>
+			<Questioncontainer>
+				{/*{answers[currentQuestionIndex] ? answers[currentQuestionIndex].isCorrect : 'no answer'}*/}
+				<Questiontext>Question: {question.questionText}</Questiontext>
+				<Buttonssection>
+					{question.options.map((item, index) => (
+						<Choice>
+							<Choicebutton
+								key={item}
+								onClick={() => onAnswerSubmit(question.id, index)}
+								style={{
+									backgroundColor: !answers
+										? '#EADCA6'
+										: index === question.correctAnswerIndex
+										? '#17D7A0'
+										: '#B91646',
+								}}
+							>
+								<Chosenanswer>{item}</Chosenanswer>{' '}
+							</Choicebutton>
+						</Choice>
+					))}
+					<Nextbutton
+						disabled={!answers[currentQuestionIndex]}
+						onClick={() => dispatch(quiz.actions.goToNextQuestion())}
+					>
+						{' '}
+						Next Question
+					</Nextbutton>
+				</Buttonssection>
 
-			<Feedback>
-				{answers[currentQuestionIndex] ? (
-					answers[currentQuestionIndex].isCorrect ? (
-						<p>Woohoo! It's Correct</p>
+				<Feedback>
+					{answers[currentQuestionIndex] ? (
+						answers[currentQuestionIndex].isCorrect ? (
+							<p>Woohoo! It's Correct</p>
+						) : (
+							<p>Oh no.... It's not correct</p>
+						)
 					) : (
-						<p>Oh no.... It's not correct</p>
-					)
-				) : (
-					<p>Waiting for the answer</p>
-				)}
-			</Feedback>
+						<p>Waiting for the answer</p>
+					)}
+				</Feedback>
 
-			<div>Question {question.id} out of 8</div>
+				<div>Question {question.id} out of 8</div>
 
-			<div>Question: (8-{question.id})</div>
+				<div>Question: (8-{question.id})</div>
 
-			{/*<div>Question: (({question.id}/8)* 100)</div> 
+				{/*<div>Question: (({question.id}/8)* 100)</div> 
         <div>(`${percentage}`)</div>*/}
+			</Questioncontainer>
 		</Main>
 	);
 };
--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -1,132 +1,150 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 // Change these to your own questions!
 
-
 const initialState = {
-  questions: [
-    { id: 1, questionText: 'In order of birth, who are the seven Weasley siblings?', 
-      options: ['Percy, Bill, George, Charlie, Fred, Ron, Ginny', 
-                'George, Fred, Percy, Bill, Charlie, Ron, Ginny', 
-                'Bill, Charlie, Percy, Fred, George, Ron, Ginny'], 
-      correctAnswerIndex: 2 },
-    { id: 2, questionText: 'What is the only book in the Harry Potter series that does not feature Lord Voldemort?', 
-      options: ['The Sorceres Stone', 
-                'The Prisoner of Akzaban', 
-                'The Deathly Hallows'], 
-      correctAnswerIndex: 1 },
-      { id: 3, questionText: 'Who is Scabbers the Rat?', 
-      options: ["Peter Pettigrew", 
-                "Remus Lupin", 
-                "Sirius Black"], 
-      correctAnswerIndex: 0 },
-      { id: 4, questionText: 'Which Harry Potter word is now in the Oxford English Dictionary?', 
-      options: ["Voldemort", 
-                "Hogwarts",
-                "Muggle"], 
-      correctAnswerIndex: 2 },
-      { id: 5, questionText: 'Which of these Hogwarts Professors teaches Transfiguration?', 
-      options: ["Professor Snape", 
-                "Professor McGonagall",
-                "Professor Sprout"], 
-      correctAnswerIndex: 1 },
-      { id: 6, questionText: 'Who invented the spell Sectumsempra?', 
-      options: ["Professor Snape", 
-                "Miranda Goshawk",
-                "Lord Voldemort"], 
-      correctAnswerIndex: 0 },
-      { id: 7, questionText: 'Who is Nagini?', 
-      options: ["Hermione's Cat", 
-                "Ron's Owl",
-                "Lord Voldemort's Snake"], 
-      correctAnswerIndex: 2 },
-      { id: 8, questionText: 'How many players are there in a Quidditch team?', 
-      options: ["7", 
-                "8",
-                "9"], 
-      correctAnswerIndex: 0 }
-  ],
-  answers: [],
-  currentQuestionIndex: 0,
-  quizOver: false,
-  correctAnswers: 0 /* for the score */
-}
+	questions: [
+		{
+			id: 1,
+			questionText: 'In order of birth, who are the seven Weasley siblings?',
+			options: [
+				'Percy, Bill, George, Charlie, Fred, Ron, Ginny',
+				'George, Fred, Percy, Bill, Charlie, Ron, Ginny',
+				'Bill, Charlie, Percy, Fred, George, Ron, Ginny',
+			],
+			correctAnswerIndex: 2,
+		},
+		{
+			id: 2,
+			questionText:
+				'What is the only book in the Harry Potter series that does not feature Lord Voldemort?',
+			options: [
+				'The Sorceres Stone',
+				'The Prisoner of Akzaban',
+				'The Deathly Hallows',
+			],
+			correctAnswerIndex: 1,
+		},
+		{
+			id: 3,
+			questionText: 'Who is Scabbers the Rat?',
+			options: ['Peter Pettigrew', 'Remus Lupin', 'Sirius Black'],
+			correctAnswerIndex: 0,
+		},
+		{
+			id: 4,
+			questionText:
+				'Which Harry Potter word is now in the Oxford English Dictionary?',
+			options: ['Voldemort', 'Hogwarts', 'Muggle'],
+			correctAnswerIndex: 2,
+		},
+		{
+			id: 5,
+			questionText:
+				'Which of these Hogwarts Professors teaches Transfiguration?',
+			options: ['Professor Snape', 'Professor McGonagall', 'Professor Sprout'],
+			correctAnswerIndex: 1,
+		},
+		{
+			id: 6,
+			questionText: 'Who invented the spell Sectumsempra?',
+			options: ['Professor Snape', 'Miranda Goshawk', 'Lord Voldemort'],
+			correctAnswerIndex: 0,
+		},
+		{
+			id: 7,
+			questionText: 'Who is Nagini?',
+			options: ["Hermione's Cat", "Ron's Owl", "Lord Voldemort's Snake"],
+			correctAnswerIndex: 2,
+		},
+		{
+			id: 8,
+			questionText: 'How many players are there in a Quidditch team?',
+			options: ['7', '8', '9'],
+			correctAnswerIndex: 0,
+		},
+	],
+	answers: [],
+	currentQuestionIndex: 0,
+	quizOver: false,
+	correctAnswers: 0 /* Added correctAnswer to  */,
+};
 
 export const quiz = createSlice({
-  name: 'quiz',
-  initialState,
-  reducers: {
-
-    /**
-     * Use this action when a user selects an answer to the question.
-     * The answer will be stored in the `quiz.answers` state with the
-     * following values:
-     *
-     *    questionId  - The id of the question being answered.
-     *    answerIndex - The index of the selected answer from the question's options.
-     *    question    - A copy of the entire question object, to make it easier to show
-     *                  details about the question in your UI.
-     *    answer      - The answer string.
-     *    isCorrect   - true/false if the answer was the one which the question says is correct.
-     *
-     * When dispatching this action, you should pass an object as the payload with `questionId`
-     * and `answerIndex` keys. See the readme for more details.
-     */
-    submitAnswer: (state, action) => {
-      console.log('State: ', state);
+	name: 'quiz',
+	initialState,
+	reducers: {
+		/**
+		 * Use this action when a user selects an answer to the question.
+		 * The answer will be stored in the `quiz.answers` state with the
+		 * following values:
+		 *
+		 *    questionId  - The id of the question being answered.
+		 *    answerIndex - The index of the selected answer from the question's options.
+		 *    question    - A copy of the entire question object, to make it easier to show
+		 *                  details about the question in your UI.
+		 *    answer      - The answer string.
+		 *    isCorrect   - true/false if the answer was the one which the question says is correct.
+		 *
+		 * When dispatching this action, you should pass an object as the payload with `questionId`
+		 * and `answerIndex` keys. See the readme for more details.
+		 */
+		submitAnswer: (state, action) => {
+			console.log('State: ', state);
 			console.log('Action: ', action);
 
-      const { questionId, answerIndex } = action.payload
-      const question = state.questions.find((q) => q.id === questionId)
+			const { questionId, answerIndex } = action.payload;
+			const question = state.questions.find((q) => q.id === questionId);
 
-      if (!question) {
-        throw new Error('Could not find question! Check to make sure you are passing the question id correctly.')
-      }
+			if (!question) {
+				throw new Error(
+					'Could not find question! Check to make sure you are passing the question id correctly.'
+				);
+			}
 
-      if (question.options[answerIndex] === undefined) {
-        throw new Error(`You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`)
-      }
+			if (question.options[answerIndex] === undefined) {
+				throw new Error(
+					`You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
+				);
+			}
 
-      state.answers.push({
-        questionId,
-        answerIndex,
-        question,
-        answer: question.options[answerIndex],
-        isCorrect: question.correctAnswerIndex === answerIndex
-      })
-     
-    },
+			state.answers.push({
+				questionId,
+				answerIndex,
+				question,
+				answer: question.options[answerIndex],
+				isCorrect: question.correctAnswerIndex === answerIndex,
+			});
+		},
 
+		/**
+		 * Use this action to progress the quiz to the next question. If there's
+		 * no more questions (the user was on the final question), set `quizOver`
+		 * to `true`.
+		 *
+		 * This action does not require a payload.
+		 */
+		goToNextQuestion: (state) => {
+			if (state.currentQuestionIndex + 1 === state.questions.length) {
+				state.quizOver = true;
+			} else {
+				state.currentQuestionIndex += 1;
+			}
+		},
 
-    /**
-     * Use this action to progress the quiz to the next question. If there's
-     * no more questions (the user was on the final question), set `quizOver`
-     * to `true`.
-     *
-     * This action does not require a payload.
-     */
-    goToNextQuestion: (state) => {
-      if (state.currentQuestionIndex + 1 === state.questions.length) {
-        state.quizOver = true
-      } else {
-        state.currentQuestionIndex += 1
-      }
-    },
-
-    /**
-     * Use this action to reset the state to the initial state the page had
-     * when it was loaded. Who doesn't like re-doing a quiz when you know the
-     * answers?!
-     *
-     * This action does not require a payload.
-     */
-     restart: () => {
-      return initialState
-    },
-    /* raise the score for every correct answer */
-    addScore: (state) => {
-      state.correctAnswers += 1
-    }
-
-  }
-})
+		/**
+		 * Use this action to reset the state to the initial state the page had
+		 * when it was loaded. Who doesn't like re-doing a quiz when you know the
+		 * answers?!
+		 *
+		 * This action does not require a payload.
+		 */
+		restart: () => {
+			return initialState;
+		},
+		/* raise the score for every correct answer */
+		addScore: (state) => {
+			state.correctAnswers += 1;
+		},
+	},
+});
