10b417e2f831b62524c0018bf7f429d328bb916f
themisk84 <efthymios.karakasis@gmail.com>
2021-11-11 11:40:06 +0100

    styled counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,18 +6,26 @@ const CounterWrapper = styled.div`
   display: flex;
   text-align: center;
   justify-content: center;
-  height: 10px;
+  height: 30px;
   background-color: red;
+`;
+
+const Pragraph = styled.p`
+  margin: 0;
+  text-align: center;
 `;
 
 const Counter = () => {
   const counter = useSelector((store) => store.quiz.currentQuestionIndex);
-  const question = useSelector((state) => state.quiz.questions);
+  const question = useSelector((state) => state.quiz.questions.length);
 
   return (
-    <CounterWrapper style={{ width: `${(question.length - counter) * 10}%` }}>
-      {counter + 1}/5
-    </CounterWrapper>
+    <>
+      <CounterWrapper style={{ width: `${(question - counter) * 10}%` }}>
+        {(question - counter) * 10}%
+      </CounterWrapper>
+      <Pragraph>You have {question - counter} questions to finish</Pragraph>
+    </>
   );
 };
 
