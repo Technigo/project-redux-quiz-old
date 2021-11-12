a871934b5d8eb4ba35eca780874172cd8cfc0086
Linnea <linnea.iseb@gmail.com>
2021-11-12 15:21:13 +0100

    Fixed more errors

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,28 +4,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { quiz } from 'reducers/quiz';
 
 export const Summary = () => {
- 
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-
   const userAnswer = useSelector((state) => state.quiz.answers);
-
   const dispatch = useDispatch();
-
 
   return (
     <div className="summary">
       <h2>This is your summary</h2>
       {userAnswer.map((item) => (
         <>
-        <p>
-          {' '} 
-          {item.questionId}: Correct answer is {item.question.options[item.question.correctAnswerIndex]}, so {' '} 
-
-          {item.answer} {' '} was {item.isCorrect ? 'correct' : 'wrong'}
-        </p>
+          <p>
+            {' '}
+            {item.questionId}: Correct answer is{' '}
+            {item.question.options[item.question.correctAnswerIndex]}, so {item.answer} was{' '}
+            {item.isCorrect ? 'correct' : 'wrong'}
+          </p>
         </>
       ))}
-      <button className="btn" type="button" onClick={() => dispatch(quiz.actions.restart())}>Restart Quiz</button>
+      <button className="btn" type="button" onClick={() => dispatch(quiz.actions.restart())}>
+        Restart Quiz
+      </button>
     </div>
   );
 };
