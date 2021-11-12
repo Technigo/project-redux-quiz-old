991e654d4c32362bf2e9d50086029207994fb092
Gustav Frid <gustav.frid@gmail.com>
2021-11-12 20:37:14 +0100

    fix progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,28 +1,29 @@
-import React from "react";
-import styled from "styled-components";
-import { useSelector } from "react-redux";
+import React from 'react'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+
+const ProgressBarWrapper = styled.div`
+  width: 100%;
+  height: 10px;
+  display: flex;
+  position: absolute;
+  z-index: 1;
+`
+const Progress = styled.div`
+  width: ${props => props.width}%;
+  height: 100%;
+  background-color: rgb(232, 178, 70);
+`
 
 export const ProgressBar = () => {
-	const answers = useSelector((state) => state.quiz.answers);
-	const questions = useSelector((state) => state.quiz.questions);
-	const ProgressBarWrapper = styled.div`
-		width: 100%;
-		height: 10px;
-		display: flex;
-		position: absolute;
-		z-index: 1;
-	`;
-	const Progress = styled.div`
-		width: ${100 / questions.length}%;
-		height: 100%;
-		background-color: rgb(232, 178, 70);
-	`;
+  const answers = useSelector(state => state.quiz.answers)
+  const questions = useSelector(state => state.quiz.questions)
 
-	return (
-		<ProgressBarWrapper>
-			{answers.map((answer) => (
-				<Progress key={answer.questionId} />
-			))}
-		</ProgressBarWrapper>
-	);
-};
+  return (
+    <ProgressBarWrapper>
+      {answers.map(answer => (
+        <Progress key={answer.questionId} width={100 / questions.length} />
+      ))}
+    </ProgressBarWrapper>
+  )
+}
