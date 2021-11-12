7219695e95d521008f140614439203ef944e3a2e
NinaAlejandra <Niinaa___@hotmail.com>
2021-11-12 14:08:33 +0100

    changes in css

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,44 +1,45 @@
-import React from "react"
-import { useSelector, useDispatch } from "react-redux"
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import "./CurrentQuestion.css" // imported CSS
+import "./CurrentQuestion.css"; // imported CSS
 
-import { quiz } from "../reducers/quiz"
-import { Summary } from "./Summary" // 👈 here is the import of Summary.js -> se line 33
+import { quiz } from "../reducers/quiz";
+import { Summary } from "./Summary"; // 👈 here is the import of Summary.js -> se line 33
 
 export const CurrentQuestion = () => {
   const question = useSelector(
     (store) => store.quiz.questions[store.quiz.currentQuestionIndex]
-  )
+  );
 
   const answer = useSelector(
     (store) => store.quiz.answers.find((a) => a.questionId === question.id) // 👈 this selector fetches the answer to a question (found it in the brief!)
-  )
-  const quizOver = useSelector((store) => store.quiz.quizOver) // 👈 This selector gets the functions that will be able to render the <Summary /> when all Qs are answered
+  );
+  const quizOver = useSelector((store) => store.quiz.quizOver); // 👈 This selector gets the functions that will be able to render the <Summary /> when all Qs are answered
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   const onAnswerSubmit = (id, index) => {
-    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }))
-  }
+    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }));
+  };
 
   const onNextButtonClick = () => {
-    dispatch(quiz.actions.goToNextQuestion())
-  }
+    dispatch(quiz.actions.goToNextQuestion());
+  };
 
   // 👇 Here is the selected quizOver that executes when al Qs are answered.
   if (quizOver === true) {
-    return <Summary />
+    return <Summary />;
   }
 
   return (
     <>
       <div className="question-wrapper">
         <h2>{question.questionText}</h2>
+        {/* //* <img src={question.img} alt="great woman in computer science" /> */}
       </div>
       <div className="button-wrapper">
         {question.options.map((item, index) => (
@@ -65,5 +66,5 @@ export const CurrentQuestion = () => {
         <p className="counter">{question.id} / 5</p>
       </div>
     </>
-  )
-}
+  );
+};
