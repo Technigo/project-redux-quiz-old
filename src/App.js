a25fef519d651a43d35d6c388aadae10c346ce0b
Gustav Frid <gustav.frid@gmail.com>
2021-11-12 21:12:34 +0100

    move redirects

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export const App = () => {
           <Route path='/' element={<StartPage />} />
           <Route path='/quiz' element={<CurrentQuestion />} />
           <Route path='/summary' element={<Summary />} />
-          <Route path='/*' element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Provider>
     </BrowserRouter>
