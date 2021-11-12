c8bf2cfefb4fb15e55ee9b742930269cd491fffc
Gustav Frid <gustav.frid@gmail.com>
2021-11-12 21:08:17 +0100

    added redirect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { quiz } from "reducers/quiz";
-import { StartPage } from "components/StartPage";
-import { Summary } from "components/Summary";
-import { CurrentQuestion } from "components/CurrentQuestion";
-import { ProgressBar } from "components/ProgressBar";
-import { NotFound } from "components/NotFound";
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { quiz } from 'reducers/quiz'
+import { StartPage } from 'components/StartPage'
+import { Summary } from 'components/Summary'
+import { CurrentQuestion } from 'components/CurrentQuestion'
+import { ProgressBar } from 'components/ProgressBar'
+import { NotFound } from 'components/NotFound'
 
 const reducer = combineReducers({
-	quiz: quiz.reducer,
-});
+  quiz: quiz.reducer,
+})
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer })
 
 export const App = () => {
-	return (
-		<BrowserRouter>
-			<Provider store={store}>
-				<ProgressBar />
-				<Routes>
-					<Route path="/" element={<StartPage />} />
-					<Route path="/quiz" element={<CurrentQuestion />} />
-					<Route path="/summary" element={<Summary />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</Provider>
-		</BrowserRouter>
-	);
-};
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <ProgressBar />
+        <Routes>
+          <Route path='/' element={<StartPage />} />
+          <Route path='/quiz' element={<CurrentQuestion />} />
+          <Route path='/summary' element={<Summary />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  )
+}
