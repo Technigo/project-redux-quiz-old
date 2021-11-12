d3cbea6df9e9c67daf5605463e5e448dea2425de
Rephili <72921631+Rephili@users.noreply.github.com>
2021-11-12 14:32:50 +0100

    added readme

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # Redux quiz group project
 
-Replace this readme with your own information about your project.
+Project Redux Quiz
+By Birgit Nehrwein, Darya Lapata, Aschwin Siaila, Jessica Hermansson and Rebecca Philipson
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This group project consisted of building a quiz game with Redux toolkit in React. It is a multiple choice quiz with five questions. When the user selects an answer a message will be shown if it was correct or not. We also implemented a progress bar showing how many questions are left to be answered during the game. The quiz also has a start page and a summary page that shows the amount of correct answers.
+The goal of this project was to learn about Redux and global state and how to interact with our store using selectors or by dispatching actions.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We worked with the project together as a group as well as in pairs and individually (in smaller branches) and the collaboration worked out very well. One of the major challenges we faced was the planning of the project. Since it was our first time working with Redux we needed to take one step at the time and explore the outcome of the new chunks of code before moving to the next task. Therefore it was hard to plan a lot of the work in advance. If we would start all over again we would focus more on the funcionality and less on the styling for the project.
 
 ## View it live
 
 https://friendly-northcutt-380f3a.netlify.app/
-
-
-
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,58 +1,102 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 
+// styled components for the start page
 
+const StartPageContainer = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(12, 1fr);
+  justify-content: center;
+  place-items: center;
+  background-color: rgb(245, 245, 245);
+`;
 
+const StartButton = styled.button`
+  grid-column: 3;
+  grid-row: 6;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  background-color: transparent;
+  border: none;
+  margin-top: 20px;
+`;
 
+const StartButtonText = styled.div`
+  height: 55px;
+  width: 150px;
+  background-color: rgb(236, 227, 227);
+  color: rgb(61, 61, 61);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  border-radius: 20px;
+  box-shadow: inset 0 0 2px rgba(255, 255, 255, 0.05),
+    inset 4px 4px 6px rgba(0, 0, 0, 0.2);
+  color: yellow;
+  text-shadow: 0 0 2px rgb(78, 77, 77), 0 0 15px yellow, 0 0 25px yellow;
+  animation: glow 1.5s linear infinite;
+  cursor: pointer;
+`;
+const Ul = styled.ul`
+  grid-column: 3;
+  grid-row: 5;
+  width: 100%;
+  display: flex;
+  padding: 0;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+`;
 
+const Li = styled.li`
+  list-style: none;
+`;
 
-
-
+const Checkbox = styled.input`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 50px;
+  width: 50px;
+  z-index: 100;
+`;
 
 const StartPage = () => {
   const history = useHistory();
 
   return (
-   
-     
-       
-         
-          
-  <main className="StartPageContainer">
-            
-            
-          <ul>
-  <li>
-    <input type="checkbox" />
-    <div className="startText" >Q</div>
-  </li>
-  <li>
-    <input type="checkbox" />
-    <div className="startText">U</div>
-  </li>
-  <li>
-    <input type="checkbox" />
-    <div className="startText">I</div>
-  </li>
-  <li>
-    <input type="checkbox" />
-    <div className="startText">Z</div>
-  </li>
-  
-    
-</ul>
-            
-            
-            
-            
+    <StartPageContainer>
+      <Ul>
+        <Li>
+          <Checkbox />
+          <div className="startText">Q</div>
+        </Li>
+        <Li>
+          <input type="checkbox" />
+          <div className="startText">U</div>
+        </Li>
+        <Li>
+          <input type="checkbox" />
+          <div className="startText">I</div>
+        </Li>
+        <Li>
+          <input type="checkbox" />
+          <div className="startText">Z</div>
+        </Li>
+      </Ul>
 
-            <button className="startBtn" type="button" onClick={() => history.push("/game")}>
-            <div className="startText-btn">START</div>
-            </button>
-          
-        </main>
-      
-    
+      <StartButton type="button" onClick={() => history.push("/game")}>
+        <StartButtonText>START</StartButtonText>
+      </StartButton>
+    </StartPageContainer>
   );
 };
 
