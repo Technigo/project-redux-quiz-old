efc353720d1c2e14080992e73d0c360ef4ab4221
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2022-01-11 08:37:51 +0100

    Fixed identation

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import './StartPage.css';
 
 export const StartPage = () => {
-    return (
-      <main className="background-startpage">
-        <Link to="/quiz">
-          <button className="start-button">START QUIZ HERE!</button>
+  return (
+    <main className="background-startpage">
+      <Link to="/quiz">
+        <button className="start-button">START QUIZ HERE!</button>
         </Link>
       </main>
     );
