183679cdf6ee1219d4e37f4e10373684f7c789e4
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-04-04 14:32:59 +0200

    added buttons answer options

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const question = useSelector(
+    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+  );
+  //forwarding data
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+      {question.options.map((item) => {
+        return <button key={item}>{item}</button>;
+      })}
     </div>
-  )
-}
+  );
+};
