7121fe64f833dcfd55dec11de1aa9a2c4490969b
Arianna Allahyar <anallahyar@gmail.com>
2022-04-04 16:32:00 +0200

    Data object created

---
+++ src/data.js
@@ -0,0 +1,57 @@
+const questions = [
+  {
+    id: "01",
+    question_text: "Surgical face masks",
+    input_type: "radio",
+    options: [
+			{ value: "Recyling station", label: "Recyling station" },
+			{ value: "Plastic", label: "Plastic" },
+      { value: "Household trash", label: "Household trash" },
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    id: "02",
+    question_text: "Styrafoam",
+    input_type: "radio",
+    options: [
+			{ value: "Carton and cardboard", label: "Carton and cardboard" },
+      { value: "Plastic", label: "Plastic" },
+      { value: "Household garbage", label: "Household garbage" },
+    ],
+    correctAnswerIndex: 3,
+  },
+  {
+    id: "03",
+    question_text: "Baking paper",
+    input_type: "radio",
+    options: [
+      { value: "Paper", label: "Paper" },
+      { value: "Plastic", label: "Plastic" },
+      { value: "Household garbage", label: "Household garbage" },
+    ],
+    correctAnswerIndex: 2,
+  },
+  {
+    id: "04",
+    question_text: "Chip bags",
+    input_type: "radio",
+    options: [
+      { value: "Paper", label: "Paper" },
+      { value: "Plastic", label: "Plastic" },
+      { value: "Household garbage", label: "Household garbage" },
+    ],
+    correctAnswerIndex: 1,
+  },
+  {
+    id: "05",
+    question_text: "Coffee filters",
+    input_type: "text",
+    options: [
+      { value: "Paper", label: "Paper" },
+      { value: "Plastic", label: "Plastic" },
+      { value: "Household garbage", label: "Household garbage" },
+    ],
+    correctAnswerIndex: 3,
+  },
+];
