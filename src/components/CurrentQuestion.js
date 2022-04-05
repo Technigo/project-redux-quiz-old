1c1d8a7374da93c070fa321349774fb7623373d0
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-04-05 14:45:34 +0200

    .

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
 // import { quiz } from 'reducers/quiz'
