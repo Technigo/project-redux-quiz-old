87798baa2614d0e19ed93b8229be6899f3af5b3f
Emma Högberg <emmahogberg88@gmail.com>
2022-04-05 18:43:57 +0200

    add welcome page

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,23 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import QuestionPage from './QuestionPage';
-import QuizOver from './QuizOver';
-
+import QuestionPage from './QuestionPage'
+import QuizOver from './QuizOver'
 
 export const CurrentQuestion = () => {
+	const gameOver = useSelector((state) => state.quiz.quizOver)
+	const question = useSelector(
+		(state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+	)
 
+	if (!question) {
+		return <h1>Oh no! I could not find the current question!</h1>
+	}
 
-    const gameOver = useSelector((state) => state.quiz.quizOver);
-    const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-
-
-    if (!question) {
-      return <h1>Oh no! I could not find the current question!</h1>
-    }
-
-    return (
-      <main>
-        {!gameOver ? <QuestionPage /> : <QuizOver />}
-      </main>
-    )
+	return <main>{!gameOver ? <QuestionPage /> : <QuizOver />}</main>
 }
--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,62 +1,62 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { quiz } from "reducers/quiz";
-import styled from "styled-components";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from 'reducers/quiz'
+import styled from 'styled-components'
 
 const AnswerSubmitBtn = styled.button`
-  &:active {
-    background: ${(props) => props.background};
-  }
-`;
+	&:active {
+		background: ${(props) => props.background};
+	}
+`
 
 const QuestionPage = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch()
 
-  const question = useSelector(
-    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
-  );
-  const answer = useSelector((state) =>
-    state.quiz.answers.find((a) => a.questionId === question.id)
-  );
+	const question = useSelector(
+		(state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+	)
+	const answer = useSelector((state) =>
+		state.quiz.answers.find((a) => a.questionId === question.id)
+	)
 
-  const answerArray = useSelector((state) => state.quiz.answers);
-  console.log("answerArray", answer);
+	const answerArray = useSelector((state) => state.quiz.answers)
+	console.log('answerArray', answer)
 
-  const onAnswerSubmit = (id, index) => {
-    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }));
-  };
-  const displayNextQuestion = () => {
-    dispatch(quiz.actions.goToNextQuestion());
-  };
+	const onAnswerSubmit = (id, index) => {
+		dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }))
+	}
+	const displayNextQuestion = () => {
+		dispatch(quiz.actions.goToNextQuestion())
+	}
 
-  //setTimeout(displayNextQuestion, 2000);
+	//setTimeout(displayNextQuestion, 2000);
 
-  //Check the answer state - correct or incorrect
+	//Check the answer state - correct or incorrect
 
-  return (
-    <section className="question-section">
-      <h1>Guess Whose Butt</h1>
-      <div className="question-container">
-        <img
-          className="question-img"
-          src={question.img}
-          alt={question.img_atl}
-        />
-        <div className="answer-btn-container">
-          {question.options.map((item, index) => (
-            <AnswerSubmitBtn
-              type="submit"
-              onClick={() => onAnswerSubmit(question.id, index)}
-              background={answer?.isCorrect ? "blue" : "red"}
-              key={item}
-            >
-              {item}
-            </AnswerSubmitBtn>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section className='question-section'>
+			<h1>Guess Whose Butt</h1>
+			<div className='question-container'>
+				<img
+					className='question-img'
+					src={question.img}
+					alt={question.img_atl}
+				/>
+				<div className='answer-btn-container'>
+					{question.options.map((item, index) => (
+						<AnswerSubmitBtn
+							type='submit'
+							onClick={() => onAnswerSubmit(question.id, index)}
+							background={answer?.isCorrect ? 'blue' : 'red'}
+							key={item}
+						>
+							{item}
+						</AnswerSubmitBtn>
+					))}
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default QuestionPage;
+export default QuestionPage
