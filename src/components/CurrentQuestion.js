4effbac64500ed4cec2ba4bd7add00c86e92383f
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-04-06 10:00:29 +0200

    more buttons

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,70 +1,98 @@
-import React from 'react'
-// import { Link } from "react-router-dom"
-import { useSelector, useDispatch } from 'react-redux'
-import { quiz } from 'reducers/quiz'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { quiz } from 'reducers/quiz';
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-  const answer = useSelector((state) => state.quiz.answers[state.quiz.currentQuestionIndex])
-  const amountOfQuestions = useSelector((state) => state.quiz.questions.length)
-  const amountOfAnswers = useSelector((state) => state.quiz.answers.length)
-  // const renderButtons = useSelector((state) => state.currentQuestionIndex + 1 === state.questions.length)
-  const dispatch = useDispatch()
-  const isQuizOver = useSelector((state) => state.quiz.quizOver)
-   console.log("store", isQuizOver)
+	const question = useSelector(
+		(state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+	);
+	const answer = useSelector(
+		(state) => state.quiz.answers[state.quiz.currentQuestionIndex]
+	);
+	const amountOfQuestions = useSelector((state) => state.quiz.questions.length);
+	const amountOfAnswers = useSelector((state) => state.quiz.answers.length);
+	const isQuizOver = useSelector((state) => amountOfQuestions === question.id);
 
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const onAnswerSubmit = (id, index) => {
-    dispatch(quiz.actions.submitAnswer({questionId:id, answerIndex:index}))
-  }
-  
-  const clickNext = () => {
+	const onRestartButtonClick = () => {
+		dispatch(quiz.actions.restart());
+		navigate('/');
+	};
 
-      dispatch(quiz.actions.goToNextQuestion())
-  }
+	const onAnswerSubmit = (id, index) => {
+		dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }));
+	};
 
-  if (!question) {
-    return <h1>And just like that... I could not find the current question!</h1>
-  }
+	const clickNext = () => {
+		dispatch(quiz.actions.goToNextQuestion());
+	};
 
+	if (!question) {
+		return (
+			<h1>And just like that... I could not find the current question!</h1>
+		);
+	}
 
-  /*THERE ARE SOME PROBLEMS WITH THE BELOW, IT SHOWS TICK OR X FOR ALL BUTTONS */
-  return (
-    <div>
-      <h1>Question: {question.questionText}</h1>
-      {question.options.map((item, index) => (
-          <button
-            key={item}
-            onClick={() => onAnswerSubmit(question.id, index)}>
-            {item} <span> {answer?.isCorrect ? "✔️" : answer?.isCorrect === false ? "✖️"  : ""} </span>
-          </button>
-      ))}
+	let button;
 
-      {/*TEXT SHOWING IF THE ANSWER WAS RIGHT OR WRONG*/ }
-      <p>{answer?.isCorrect && "Correct answer"}</p>
-      <p>{answer?.isCorrect === false && "Wrong answer"}</p>
+	if (!isQuizOver) {
+		button = (
+			<button onClick={() => clickNext()} disabled={!answer}>
+				Next
+			</button>
+		);
+	} else {
+		button = (
+			<Link to='/summary'>
+				<button>Finish</button>
+			</Link>
+		);
+	}
 
-      {/*BUTTON TO GO TO NEXT QUESTION*/ }
+	/*THERE ARE SOME PROBLEMS WITH THE BELOW, IT SHOWS TICK OR X FOR ALL BUTTONS */
+	return (
+		<div>
+			<h1>
+				Question {question.id}: {question.questionText}
+			</h1>
+			{question.options.map((item, index) => (
+				<button key={item} onClick={() => onAnswerSubmit(question.id, index)}>
+					{item}{' '}
+					<span>
+						{' '}
+						{answer?.isCorrect
+							? '✔️'
+							: answer?.isCorrect === false
+							? '✖️'
+							: ''}{' '}
+					</span>
+				</button>
+			))}
 
-      
-      {/* { if (state.quizOver = false) { */}
-         <button
-            onClick={() => clickNext()}
-            disabled={!answer}>
-              Next
-          </button> 
-        
-      {/* } else if (state.quizOver = true) {
-        <Link to="/summary">
-            <button>Finish</button>
-        </Link>
-        
-      }} */}
-      
+			{/*TEXT SHOWING IF THE ANSWER WAS RIGHT OR WRONG*/}
+			<p>{answer?.isCorrect && 'Correct answer'}</p>
+			<p>{answer?.isCorrect === false && 'Wrong answer'}</p>
 
-      {/*PROGRESS BAR*/ }
-      <label htmlFor='Progressbar'>{question.id} / {amountOfQuestions}</label>
-      <progress id="Progressbar" value={amountOfAnswers} min="0" max="5"></progress>
-    </div>
-  )
-}
+			{/*BUTTON TO GO TO NEXT QUESTION*/}
+			{button}
+
+			{/*RESTART BUTTON*/}
+			<button onClick={onRestartButtonClick}>Restart</button>
+
+			{/*PROGRESS BAR*/}
+			<label htmlFor='Progressbar'>
+				{question.id} / {amountOfQuestions}
+			</label>
+			<progress
+				id='Progressbar'
+				value={amountOfAnswers}
+				min='0'
+				max='5'></progress>
+		</div>
+	);
+};
--- src/components/SummaryPage.js
+++ src/components/SummaryPage.js
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const SummaryPage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return <div></div>;
+};
 
-
-export default SummaryPage    
+export default SummaryPage;
