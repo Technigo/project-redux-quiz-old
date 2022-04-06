973df65d3599d0506ef2e01c7c74436e9026101e
Therese Nyman <theresenyman@Thereses-MacBook-Air.local>
2022-04-06 10:30:06 +0200

    added restart dispatch

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSelector, useDispatch } from 'react-redux'
 import { QuestionBackground } from 'components/CurrentQuestion'
+import { quiz } from 'reducers/quiz'
 
 const SummaryStyling = styled.div`
     margin: 0;
@@ -10,12 +12,18 @@ const SummaryStyling = styled.div`
 `
 
 export const Summary = () => {
+  const dispatch = useDispatch()
+
+  const onRestartButton = () => {
+    dispatch(quiz.actions.restart())
+  }
+
   return (
     <QuestionBackground>
       <SummaryStyling>
         <div>
           <h1>Good work! Here is your summary!</h1>
-          <button type="button">Restart</button>
+          <button type="button" onCli>Restart</button>
         </div>
       </SummaryStyling>
     </QuestionBackground>
