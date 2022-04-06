e9506395e54a324cf1a6bc9418f9f041d5160181
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-04-06 15:28:23 +0200

    netlify check

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { quiz } from "reducers/quiz";
+import { useSelector } from "react-redux";
 
 const Summary = () => {
-  const dispatch = useDispatch();
   const answers = useSelector((state) => state.quiz.answers);
   console.log(answers);
 
