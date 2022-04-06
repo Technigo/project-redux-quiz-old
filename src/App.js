40e005fd62a12c0f2a8fb122d8e0c46d53c85398
Emma Lindell <emma.lindell4@gmail.com>
2022-04-06 12:43:22 +0200

    Created StartPage.js and added it to App.js in Route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { quiz } from 'reducers/quiz'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import StartPage from 'components/StartPage'
 import { CurrentQuestion } from 'components/CurrentQuestion'
 
 const reducer = combineReducers({
@@ -13,8 +15,15 @@ const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <CurrentQuestion />
+    <main>
+      <Provider store={store}>
+          <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<StartPage />} />
+                <Route path="/quiz" element={<CurrentQuestion />}/>
+              </Routes>
+      </BrowserRouter>
     </Provider>
+    </main>
   )
 }
