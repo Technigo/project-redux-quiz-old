0d6a93154d9ae327c523b3f771166e8531c26851
Lisen Lundgren <lisen@Lisens-MBP.lan>
2022-04-06 10:56:24 +0200

    structure of CurrentQuestion.js

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { quiz } from 'reducers/quiz';
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+	const question = useSelector(
+		(state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+	);
+	console.log(question);
 
-  if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
-  }
+	// const now = (
+	// 	question.id
+	// 	 / 5) * 100;
+	// 	  const progressInstance = <ProgressBar now={now} label={`${now}%`} />;
 
-  return (
-    <div>
-      <h1>Question: {question.questionText}</h1>
-    </div>
-  )
-}
+	const dispatch = useDispatch();
+	const onAnswerSubmit = (id, index) => {
+		dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }));
+		if (question.correctAnswerIndex === index) {
+			alert('Correct answer');
+			dispatch(quiz.actions.goToNextQuestion());
+		} else {
+			alert('WRONG!!!');
+			dispatch(quiz.actions.goToNextQuestion());
+		}
+	};
+	console.log(questions.answers);
+
+	if (!question) {
+		return <h1>Oh no! I could not find the current question!</h1>;
+	}
+
+	return (
+		<div>
+			<h1>Question: {question.questionText}</h1>
+			{question.options.map((item, index) => {
+				return (
+					<button key={item} onClick={() => onAnswerSubmit(question.id, index)}>
+						{item}
+					</button>
+				);
+			})}
+			<p>{question.id}/5</p>
+		</div>
+	);
+};
