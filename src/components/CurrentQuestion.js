4ddb7bf9a56e4f429f04c0bd8dfccb64d5a6ff99
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-04-06 10:26:10 +0200

    updated to next question

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-
+import { quiz } from "reducers/quiz";
 export const CurrentQuestion = () => {
+  //getting data from the store
   const question = useSelector(
     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
   );
-  //forwarding data
+  //
+  const store = useSelector((state) => state);
+  console.log(store);
+  console.log(question);
+  //
 
+  //forwarding data to the store
+  const dispatch = useDispatch();
+  const onAnswersubmit = (id, index) => {
+    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }));
+    dispatch(quiz.actions.goToNextQuestion());
+  };
+  //////
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
   }
@@ -14,8 +26,12 @@ export const CurrentQuestion = () => {
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
-      {question.options.map((item) => {
-        return <button key={item}>{item}</button>;
+      {question.options.map((item, index) => {
+        return (
+          <button onClick={() => onAnswersubmit(question.id, index)} key={item}>
+            {item}
+          </button>
+        );
       })}
     </div>
   );
