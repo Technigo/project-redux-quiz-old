092563ef201562088daed64455cc50922419076f
EmmaaBerg <emmaberg0819@gmail.com>
2022-04-06 08:51:07 +0200

    displayed question and answers on summary page

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,22 +3,34 @@ import {quiz} from 'reducers/quiz'
 import { useSelector, useDispatch } from 'react-redux'
 
 const Summary = () => {
-    const answers = useSelector((state) => state.quiz.answers[state.quiz.answer])
+    const userAnswer = useSelector((state) => state.quiz.answers);
     const dispatch = useDispatch()
 
     const clickRestart = () => {
         dispatch(quiz.actions.restart())
     }
 
-    return(
-    <div>
-      <p>Hello</p>
-      <button onClick={clickRestart}>Restart</button>
-    </div>
-
-
-    )
-}
-
-export default Summary
+    return (
+      <div className='summary'>
+        <h2>This is your summary</h2>
+        {userAnswer.map((answer) => (
+          <>
+            <p>
+              {' '}
+              {answer.questionId}: Correct answer:
+              {answer.question.options[answer.question.correctAnswerIndex]} Your
+              answer:
+              {answer.answer}
+            </p>
+          </>
+        ))}
+        <button className='btn restart' type='button' onClick={clickRestart}>
+          Restart Quiz
+        </button>
+      </div>
+    );
+  };
+  
+  export default Summary;
+  
 
