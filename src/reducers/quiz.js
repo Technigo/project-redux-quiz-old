13ea1e1a64a0d979a8dd227832bca3df22c40695
Emma Högberg <emmahogberg88@gmail.com>
2022-04-06 15:18:59 +0200

    change name in question object

--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 import ElephantButt from '../components/assets/elephant_butt.png'
 import TigerButt from '../components/assets/tiger_butt.png'
 import FoxButt from '../components/assets/fox_butt.png'
@@ -14,152 +14,149 @@ import hippoImg from '../components/assets/hippo.png'
 
 // Change these to your own questions!
 const questions = [
-  {
-    id: 1,
-    options: ["Hippo", "Elephant", "Seal", "Rhino"],
-    correctAnswerIndex: 1,
-    img_alt: "elephant",
-    img: ElephantButt,
-    sticker: elephantImg,
-  },
-  {
-    id: 2,
-    options: ["Tiger", "Zebra", "Hyena", "Cheetah"],
-    correctAnswerIndex: 0,
-    img_alt: "Tiger",
-    img: TigerButt,
-    img: TigerButt,
-    sticker: tigerImg
-  },
-  {
-    id: 3,
-    options: ["Wolf", "Hyena", "Coyote", "Fox"],
-    correctAnswerIn: 3,
-    img: FoxButt,
-    img_alt: "Fox",
-    img: FoxButt,
-    sticker: foxImg
-  },
-  {
-    id: 4,
-    options: ["Puma","Jaguar", "Lion", "Tiger"],
-    img: LionButt,
-    correctAnswerIndex: 2,
-    img_alt: "Lion",
-    img: LionButt,
-    sticker: lionImg
-  },
-  {
-    id: 5,
-    img: TurtleButt,
-    options: ["Turtle", "Snake", "Crocodile", "Lizard"],
-    correctAnswerIndex: 0,
-    img_alt: "Turtle",
-    img: TurtleButt,
-    sticker: turtleImg
-  },
-  {
-    img: HippoButt,
-    id: 6,
-    options: ["Bear", "elephant", "Mamut", "Hippo"],
-    correctAnswerIndex: 3,
-    img_alt: "Hippo",
-    img: HippoButt,
-    sticker: hippoImg
-  },
-];
+	{
+		id: 1,
+		options: ['Hippo', 'Elephant', 'Seal', 'Rhino'],
+		correctAnswerIndex: 1,
+		img_alt: 'elephant',
+		img: ElephantButt,
+		sticker: elephantImg,
+	},
+	{
+		id: 2,
+		options: ['Tiger', 'Zebra', 'Hyena', 'Cheetah'],
+		correctAnswerIndex: 0,
+		img_alt: 'Tiger',
+		img: TigerButt,
+		img: TigerButt,
+		sticker: tigerImg,
+	},
+	{
+		id: 3,
+		options: ['Wolf', 'Hyena', 'Coyote', 'Fox'],
+		correctAnswerIndex: 3,
+		img: FoxButt,
+		img_alt: 'Fox',
+		img: FoxButt,
+		sticker: foxImg,
+	},
+	{
+		id: 4,
+		options: ['Puma', 'Jaguar', 'Lion', 'Tiger'],
+		img: LionButt,
+		correctAnswerIndex: 2,
+		img_alt: 'Lion',
+		img: LionButt,
+		sticker: lionImg,
+	},
+	{
+		id: 5,
+		img: TurtleButt,
+		options: ['Turtle', 'Snake', 'Crocodile', 'Lizard'],
+		correctAnswerIndex: 0,
+		img_alt: 'Turtle',
+		img: TurtleButt,
+		sticker: turtleImg,
+	},
+	{
+		img: HippoButt,
+		id: 6,
+		options: ['Bear', 'elephant', 'Mamut', 'Hippo'],
+		correctAnswerIndex: 3,
+		img_alt: 'Hippo',
+		img: HippoButt,
+		sticker: hippoImg,
+	},
+]
 
 const initialState = {
-  questions,
-  answers: [],
-  currentQuestionIndex: 0,
-  quizOver: false,
-  counter: 0,
-  animation: false
-
-};
+	questions,
+	answers: [],
+	currentQuestionIndex: 0,
+	quizOver: false,
+	counter: 0,
+	animation: false,
+}
 
 export const quiz = createSlice({
-  name: "quiz",
-  initialState,
-  reducers: {
-    /**
-     * Use this action when a user selects an answer to the question.
-     * The answer will be stored in the `quiz.answers` state with the
-     * following values:
-     *
-     *    questionId  - The id of the question being answered.
-     *    answerIndex - The index of the selected answer from the question's options.
-     *    question    - A copy of the entire question object, to make it easier to show
-     *                  details about the question in your UI.
-     *    answer      - The answer string.
-     *    isCorrect   - true/false if the answer was the one which the question says is correct.
-     *
-     * When dispatching this action, you should pass an object as the payload with `questionId`
-     * and `answerIndex` keys. See the readme for more details.
-     */
-    submitAnswer: (state, action) => {
-      const { questionId, answerIndex } = action.payload;
+	name: 'quiz',
+	initialState,
+	reducers: {
+		/**
+		 * Use this action when a user selects an answer to the question.
+		 * The answer will be stored in the `quiz.answers` state with the
+		 * following values:
+		 *
+		 *    questionId  - The id of the question being answered.
+		 *    answerIndex - The index of the selected answer from the question's options.
+		 *    question    - A copy of the entire question object, to make it easier to show
+		 *                  details about the question in your UI.
+		 *    answer      - The answer string.
+		 *    isCorrect   - true/false if the answer was the one which the question says is correct.
+		 *
+		 * When dispatching this action, you should pass an object as the payload with `questionId`
+		 * and `answerIndex` keys. See the readme for more details.
+		 */
+		submitAnswer: (state, action) => {
+			const { questionId, answerIndex } = action.payload
 
-      const question = state.questions.find((q) => q.id === questionId);
+			const question = state.questions.find((q) => q.id === questionId)
 
-      if (!question) {
-        throw new Error(
-          "Could not find question! Check to make sure you are passing the question id correctly."
-        );
-      }
+			if (!question) {
+				throw new Error(
+					'Could not find question! Check to make sure you are passing the question id correctly.'
+				)
+			}
 
-      if (question.options[answerIndex] === undefined) {
-        throw new Error(
-          `You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
-        );
-      }
+			if (question.options[answerIndex] === undefined) {
+				throw new Error(
+					`You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
+				)
+			}
 
+			//state.nextQuestion = true
 
-      //state.nextQuestion = true
-       
-     // if (question.correctAnswerIndex !== answerIndex) {
-      //   state.color = 'red'
-      //} else {
-       //  state.color = 'green'
+			// if (question.correctAnswerIndex !== answerIndex) {
+			//   state.color = 'red'
+			//} else {
+			//  state.color = 'green'
 
-      //}
-    
-      state.answers.push({
-        questionId,
-        answerIndex,
-        question,
-        answer: question.options[answerIndex],
-        isCorrect: question.correctAnswerIndex === answerIndex,
-      });
+			//}
 
-    },
+			state.answers.push({
+				questionId,
+				answerIndex,
+				question,
+				answer: question.options[answerIndex],
+				isCorrect: question.correctAnswerIndex === answerIndex,
+			})
+		},
 
-    /**
-     * Use this action to progress the quiz to the next question. If there's
-     * no more questions (the user was on the final question), set `quizOver`
-     * to `true`.
-     *
-     * This action does not require a payload.
-     */
-    goToNextQuestion: (state) => {
-      if (state.currentQuestionIndex + 1 === state.questions.length) {
-        state.quizOver = true;
-      } else {
-        state.currentQuestionIndex += 1;
-        state.nextQuestion = false;
-      }
-    },
+		/**
+		 * Use this action to progress the quiz to the next question. If there's
+		 * no more questions (the user was on the final question), set `quizOver`
+		 * to `true`.
+		 *
+		 * This action does not require a payload.
+		 */
+		goToNextQuestion: (state) => {
+			if (state.currentQuestionIndex + 1 === state.questions.length) {
+				state.quizOver = true
+			} else {
+				state.currentQuestionIndex += 1
+				state.nextQuestion = false
+			}
+		},
 
-    /**
-     * Use this action to reset the state to the initial state the page had
-     * when it was loaded. Who doesn't like re-doing a quiz when you know the
-     * answers?!
-     *
-     * This action does not require a payload.
-     */
-    restart: () => {
-      return initialState;
-    },
-  },
-});
+		/**
+		 * Use this action to reset the state to the initial state the page had
+		 * when it was loaded. Who doesn't like re-doing a quiz when you know the
+		 * answers?!
+		 *
+		 * This action does not require a payload.
+		 */
+		restart: () => {
+			return initialState
+		},
+	},
+})
