2812d53c60e6ad2b54ec2d0a181152766fccd39f
Arianna Allahyar <anallahyar@gmail.com>
2022-04-06 16:37:15 +0200

    New value added to data object for summary

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ export const questions = [
   {
     id: "01",
     questionText: "Face mask",
+    material: "Face mask",
     image: "./images/mask-2.jpg",
     options: [
       { value: "recyling station", label: "Recyling station" },
@@ -13,6 +14,7 @@ export const questions = [
   {
     id: "02",
     questionText: "Styrafoam",
+    material: "Styrafoam",
     image: "./images/frigolit-1.jpeg",
     options: [
       { value: "carton and cardboard", label: "Carton and cardboard" },
@@ -24,6 +26,7 @@ export const questions = [
   {
     id: "03",
     questionText: "Oh wow! That baking episode really turned out well! Now let’s eat the cookies and throw the baking paper in:",
+    material: "Baking paper",
     image: "./images/woman-baking.jpg",
     options: [
       { value: "paper", label: "Paper" },
@@ -35,6 +38,7 @@ export const questions = [
   {
     id: "04",
     questionText: "You know when you’ve grabbed that last piece out of the chips bag after a lazy fredagsmys. Good things are over you might think but there is still one good thing left: recycle the bag: So where do we put it?",
+    material: "Chip bag",
     image: "./images/chipsbags-2.png",
     options: [
       { value: "paper", label: "Paper" },
@@ -46,6 +50,7 @@ export const questions = [
   {
     id: "05",
     questionText: "That coffee was a winner. I wanna be a winner too. I wonder what I could do to be one? Help the man by telling him  the correct thing to do with the filter and make him a winner!",
+    material: "Coffee filter",
     image: "./images/man-coffee.jpg",
     options: [
       { value: "paper", label: "Paper" },
