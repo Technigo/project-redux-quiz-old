2e9a1e37eeab96d5c717f8dccae592c770f2d473
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-04-06 15:58:19 +0200

    latest check

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,11 +3,25 @@ import { useSelector } from "react-redux";
 
 const Summary = () => {
   const answers = useSelector((state) => state.quiz.answers);
+  //const questions = useSelector((state) => state.quiz.questions);
   console.log(answers);
-
+  let result = 0;
+  answers.forEach((answer) => {
+    console.log(answer);
+    if (answer.isCorrect) {
+      result = result + 1;
+    }
+  });
+  /* const useranswer = answers.map((answer) => answer.answerIndex);
+  const correctAnswer = questions.map(
+    (question) => question.correctAnswerIndex
+  );
+  console.log("username", useranswer);
+  console.log("correct", correctAnswer); */
   return (
     <div>
       <h1>Summary</h1>
+      <p>Your correct answers are {result} /5 </p>
     </div>
   );
 };
