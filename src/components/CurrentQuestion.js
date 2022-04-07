1c9baedbcb2868aa0052032512379941b066fa45
Lisen Lundgren <lisen@Lisens-MacBook-Pro.local>
2022-04-07 10:47:56 +0200

    import font-family and create styled components in CurrentQuestion.js

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -3,6 +3,33 @@ import { useSelector, useDispatch } from 'react-redux';
 import { quiz } from 'reducers/quiz';
 
 import swal from 'sweetalert';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	background-color: white;
+	width: 90%;
+	max-width: 40rem;
+	margin: 2rem auto;
+`;
+
+const Title = styled.h3`
+	width: 90%;
+	font-size: 1.3rem;
+`;
+
+const Image = styled.img`
+	width: 90%;
+	height: auto;
+	// object-fit: cover;
+`;
+
+const Counter = styled.p`
+	font-weight: 500;
+	font-size: 1.3rem;
+`;
 
 export const CurrentQuestion = () => {
 	const question = useSelector(
@@ -29,9 +56,9 @@ export const CurrentQuestion = () => {
 	}
 
 	return (
-		<div className='container'>
-			<h3>Question: {question.questionText}</h3>
-			<img src={question.img} alt='movie' />
+		<Container>
+			<Title>Question: {question.questionText}</Title>
+			<Image src={question.img} alt='movie' />
 			<div className='btn-container'>
 				{question.options.map((item, index) => {
 					return (
@@ -44,7 +71,7 @@ export const CurrentQuestion = () => {
 					);
 				})}
 			</div>
-			<p className='question-count'>Question {question.id} of 5</p>
-		</div>
+			<Counter>Question {question.id} of 5</Counter>
+		</Container>
 	);
 };
