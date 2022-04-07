5e4b82453f83a23ed222b17b421a143d260eb698
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-04-07 19:44:57 +0200

    styling background

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,46 +1,44 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { quiz } from 'reducers/quiz'
-import { Summary } from './Summary'
-import { ProgBar } from './ProgressBar'
-import { Header } from './Header'
-import { Footer } from './Footer'
-import styled from 'styled-components'
-import Timer from './Timer'
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { quiz } from "reducers/quiz";
+import { Summary } from "./Summary";
+import { ProgBar } from "./ProgressBar";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import styled from "styled-components";
+import Timer from "./Timer";
 
 const devices = {
-  mobile: '(min-width: 375px)',
-  tablet: '(min-width: 768px)',
-  desktop: '(min-width: 1025px)'
-}
-
+  mobile: "(min-width: 375px)",
+  tablet: "(min-width: 768px)",
+  desktop: "(min-width: 1025px)",
+};
 
 const Wrapper = styled.div`
   width: 375px;
   margin: 0 auto;
 
-  @media ${devices.tablet}{
-    width:768px;
+  @media ${devices.tablet} {
+    width: 768px;
   }
-`
+`;
 
 const Main = styled.section`
   background: rgba(0, 0, 0, 0.5);
   display: flex;
   flex-direction: column;
   border-radius: 10px;
-`
+`;
 
 const QuestionContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 const Question = styled.h1`
-  font-size: 25px;
+  font-size: 20px;
   text-align: center;
-`
+`;
 
 const Button = styled.button`
   font-family: poppins;
@@ -60,51 +58,51 @@ const Button = styled.button`
 
   border: solid 3px ${(props) => props.border};
 
-  @media ${devices.tablet}{
-    height:50px;
+  @media ${devices.tablet} {
+    height: 50px;
   }
-`
+`;
 
 export const CurrentQuestion = () => {
   // Getting data from the store
   const question = useSelector(
-    (state) => state.quiz.questions[state.quiz.currentQuestionIndex],
-  )
-  const quizOver = useSelector((state) => state.quiz.quizOver)
-  const store = useSelector((state) => state)
+    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+  );
+  const quizOver = useSelector((state) => state.quiz.quizOver);
+  const store = useSelector((state) => state);
   const currentQuestionIndex = useSelector(
-    (state) => state.quiz.currentQuestionIndex,
-  )
-  const answersArray = useSelector((state) => state.quiz.answers)
-  console.log(store)
+    (state) => state.quiz.currentQuestionIndex
+  );
+  const answersArray = useSelector((state) => state.quiz.answers);
+  console.log(store);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const onAnswerSubmit = (id, index) => {
-    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }))
-    setTimeout(displayNextQuestion, 1700)
-  }
+    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }));
+    setTimeout(displayNextQuestion, 1700);
+  };
 
   const displayNextQuestion = () => {
-    dispatch(quiz.actions.goToNextQuestion())
-  }
+    dispatch(quiz.actions.goToNextQuestion());
+  };
 
   if (!question) {
-    return <div>Oh no! I could not find the current question!</div>
+    return <div>Oh no! I could not find the current question!</div>;
   }
 
   //If-statement to change colors on button borders if right/wrong answers
 
   const changeBorderColor = (indexOption) => {
     if (answersArray.length === currentQuestionIndex) {
-      return 'none'
+      return "none";
     } else {
       if (question.correctAnswerIndex === indexOption) {
-        return 'greenyellow'
+        return "greenyellow";
       }
-      return 'red'
+      return "red";
     }
-  }
+  };
 
   return (
     <Wrapper>
@@ -128,10 +126,10 @@ export const CurrentQuestion = () => {
           </QuestionContainer>
         )}
 
-          <ProgBar />
-          <Timer />
-          <Footer />
-        </Main>
-      </Wrapper>
-  )
-}
+        <ProgBar />
+        <Timer />
+        <Footer />
+      </Main>
+    </Wrapper>
+  );
+};
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Countdown = styled.section`
   text-align: center;
   color: red;
-  font-family: 'Russo One', sans-serif;
+  font-family: "Russo One", sans-serif;
   font-size: 20px;
-`
+  margin-bottom: 20px;
+`;
 
 const Timer = () => {
-  const [counter, setCounter] = React.useState(1)
+  const [counter, setCounter] = React.useState(1);
 
   React.useEffect(() => {
     // eslint-disable-next-line no-unused-expressions
-    counter > 0 && setTimeout(() => setCounter(counter + 1), 1000)
-  }, [counter])
+    counter > 0 && setTimeout(() => setCounter(counter + 1), 1000);
+  }, [counter]);
 
-  return <Countdown>Can you beat the clock: {counter}</Countdown>
-}
-export default Timer
+  return <Countdown>Can you beat the clock: {counter}</Countdown>;
+};
+export default Timer;
