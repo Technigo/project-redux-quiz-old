6c7c06b5d891fbed2031a4199a39d95113b130fb
Tiina <tiina_liukkonen@hotmail.com>
2022-04-07 17:04:56 +0300

    question button styled component

---
+++ src/components/QuestionButton.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const QuestionButton = styled.button`
+	appearance: none;
+	width: 250px;
+	background-color: rgb(252, 252, 252);
+	border: none;
+	font-family: 'Quicksand', sans-serif;
+	font-weight: 400;
+    padding: 0.8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+`;
+
+export default QuestionButton;
