a361e4b8ed85941d3ab966e83a8b0644ea0e33ca
Emma Högberg <emmahogberg88@gmail.com>
2022-04-07 23:06:06 +0200

    add css changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import styled, {keyframes} from "styled-components";
+import styled, { keyframes } from 'styled-components'
 
 const zoomInZoomOut = keyframes`
 0% {
@@ -28,22 +28,27 @@ const zoomInZoomOut = keyframes`
 `
 
 const Button = styled.button`
-  align-self: center;
-  background: #fef8d8;
-  color: #000;
-  font-family: "Concert One", cursive;
-  border-radius: 10px;
-  padding: 10px;
-  font-size: 18px;
-  width: 10em;
-  margin-bottom: 1em;
-  border: solid ${(props) => props ? props.border : '#000'} 3px;
-  animation: ${zoomInZoomOut} ${(props) => props.animationtime} ease;
+	align-self: center;
+	background: #fef8d8;
+	text-transform: uppercase;
+	color: #000;
+	font-family: 'Concert One', cursive;
+	border-radius: 10px;
+	padding: 10px;
+	font-size: 18px;
+	width: 10em;
+	margin-bottom: 1em;
+	border: solid ${(props) => (props ? props.border : '#000')} 3px;
+	animation: ${zoomInZoomOut} ${(props) => props.animationtime} ease;
 
-  &:hover {
-    background-color: #45413c;
-    color: #fef8d8;
-  }
-`;
+	&:hover {
+		background-color: #45413c;
+		color: #fef8d8;
+	}
 
-export default Button;
+	@media (min-width: 768px) {
+		margin-bottom: 0;
+	}
+`
+
+export default Button
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -2,41 +2,39 @@ import React from 'react'
 import styled from 'styled-components'
 import { CurrentQuestion } from './CurrentQuestion'
 import { useSelector, useDispatch } from 'react-redux'
-import "./QuestionPage.css";
+import './QuestionPage.css'
 import { quiz } from 'reducers/quiz'
 import Button from './Button'
 import { Footer } from './Footer'
 
-
 export const WelcomePage = () => {
-  const dispatch = useDispatch();
-  const start = useSelector((state) => state.quiz.start);
-  const onStartButtonclick = () => {
-    dispatch(quiz.actions.startGame());
-  };
-  return (
-    <>
-      {!start ? (
-        <>
-        <main>
-          <h1>Guess Whose Butt</h1>
-          <section className="question-section">
-            <div className="question-container welcome-wrapper ">
-              <h2 className="welcome-text"> 
-               How to play?
-              </h2>
-              <p> Correct butt: +2 points </p>
-              <p>Incorrect butt: -1 point </p>
-              <p>Gameover: under 0 point</p>
-            </div>
-              <Button onClick={onStartButtonclick}> Let's play! </Button>
-          </section>
-        </main>
-        <Footer />
-        </>
-      ) : (
-        <CurrentQuestion />
-      )}
-    </>
-  );
-};
+	const dispatch = useDispatch()
+	const start = useSelector((state) => state.quiz.start)
+	const onStartButtonclick = () => {
+		dispatch(quiz.actions.startGame())
+	}
+	return (
+		<>
+			{!start ? (
+				<>
+					<main className='start'>
+						<h1>Guess Whose Butt</h1>
+						<section className='question-section start'>
+							<div className='question-container welcome-wrapper '>
+								<h2>Let’s see if you know your animal butts!</h2>
+								<h3 className='welcome-text'>How to play?</h3>
+								<p> Correct butt: +2 points </p>
+								<p>Incorrect butt: -1 point </p>
+								<p>Gameover: under 0 point</p>
+							</div>
+							<Button onClick={onStartButtonclick}> Let's play! </Button>
+						</section>
+					</main>
+					<Footer />
+				</>
+			) : (
+				<CurrentQuestion />
+			)}
+		</>
+	)
+}
