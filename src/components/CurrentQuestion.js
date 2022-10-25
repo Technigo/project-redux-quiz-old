959b008efe3f0cab90ab91a3b37884ea314ca32a
Kristin <kristin@iMac.local>
2022-10-25 12:20:20 +0200

    code along Daniel live session

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,35 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from 'reducers/quiz'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const wholeStore = useSelector((store) => store)
+  console.log(wholeStore)
+  console.log(question)
+
+  const dispacth = useDispatch();
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
 
+  const onAnswerSubmit = (questionId, answerIndex) => {
+    dispacth(quiz.actions.submitAnswer({ questionId, answerIndex }));
+    if (question.correctAnswerIndex === answerIndex) {
+      dispacth(quiz.actions.goToNextQuestion());
+    } else {
+      window.alert('you are an amateur!')
+    }
+  };
+
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+      {question.options.map((option, index) => {
+        return <button onClick={() => onAnswerSubmit(question.id, index)} key={option} type="button">{option}</button>
+      })}
     </div>
   )
 }
+
