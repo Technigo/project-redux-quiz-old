9f56d1d5e3895c25c3ff854449ce2dd414f487f6
Annika Gadman <annika.gadman@gmail.com>
2022-10-25 16:58:26 -0500

    added restart button

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from 'reducers/quiz'
 
 export const Summary = () => {
   const answers = useSelector((state) => state.quiz.answers);
   const correctAnswers = answers.filter((answer) => answer.isCorrect).length;
   const numOfAnswers = answers.length;
+  const dispatch = useDispatch();
   return (
-    <p>You got {correctAnswers} correct answers out of {numOfAnswers}</p>
+    <>
+      <p>You got {correctAnswers} correct answers out of {numOfAnswers}</p>
+      <button type="button" onClick={() => dispatch(quiz.actions.restart())}>Restart Quiz</button>
+    </>
   );
 };
