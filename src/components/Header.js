d73be966f532a1a92df75c342a90c52046c40558
Linus Eriksson <erilinus@gmail.com>
2022-10-26 10:52:14 +0200

    added description to startpage

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      <h1>Header</h1>
+      <h1>Can you outsmart an AI?</h1>
     </header>
   )
 }
--- src/components/Startpage.js
+++ src/components/Startpage.js
@@ -4,7 +4,17 @@ import styled from 'styled-components';
 const StartPage = () => {
   return (
     <StyledStartPage>
-      <p>This is the startpage</p>
+      <p>
+        In the rapid evolution of technology, AI is affecting more parts of our
+        digital lives than before. Challenging our own abilities to create. While some
+        results are comical, others are getting hard to distinguish from reality.
+      </p>
+      <p>
+        Midjourney is a research lab and the name of its program that creates images
+        from textual descriptions using machine learning. In the following pages you
+        will see some examples of the images it can create.
+      </p>
+      <p>Can you separate which images are made by Midjourney or not?</p>
       <StyledButton>
         <a href="/quiz">Start quiz</a>
       </StyledButton>
