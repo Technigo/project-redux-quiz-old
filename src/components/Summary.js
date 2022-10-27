aa1cd799e98e63dde2f5097bad22551c857533cf
Petra Eriksson <eriksson.petra@gmail.com>
2022-10-27 12:39:25 +0200

    update on summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,7 +1,7 @@
 
-import React from "react";
-import { useSelector } from "react-redux";
-import { ReturnButton } from "components/ReturnButton";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { ReturnButton } from 'components/ReturnButton';
 
 const Summary = () => {
   const summaryAnswers = useSelector((state) => state.quiz.answers);
@@ -15,28 +15,28 @@ const Summary = () => {
       <div>
         <h1>Your quiz summary</h1>
 
-          {summaryAnswers.map((results) => (
+        {summaryAnswers.map((results) => (
           <div>
             <h2>Question number: {results.questionId}</h2>
             <h3>You answered: {results.answer}</h3>
 
-          {results.isCorrect ? (
-            <h4> Your answer is correct </h4>
-          ) : (
-            <h4> Your answer is not correct </h4>
-          )}
-          {!results.isCorrect && (
-            <p>
-              Correct answer is:{" "}
-              {results.question.options[results.question.correctAnswerIndex]}
-            </p>
-          )}
-        </div>
-      ))}
-      <ReturnButton />
-    </div>
+            {results.isCorrect ? (
+              <h4> Your answer is correct </h4>
+            ) : (
+              <h4> Your answer is not correct </h4>
+            )}
+            {!results.isCorrect && (
+              <p>
+              Correct answer is:{' '}
+                {results.question.options[results.question.correctAnswerIndex]}
+              </p>
+            )}
+          </div>
+        ))}
+        <ReturnButton />
+      </div>
     </>
   );
-  };
+};
 
 export default Summary;
