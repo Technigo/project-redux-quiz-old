d6a3b9db62bb18ce997c3b9d4efbd9ba2fcb8e94
Petra Eriksson <eriksson.petra@gmail.com>
2022-10-27 12:28:35 +0200

    merge summary file

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
 
-// import { CurrentQuestion } from 'components/CurrentQuestion';
-// import { quiz } from 'reducers/quiz'
+import React from "react";
+import { useSelector } from "react-redux";
+import { ReturnButton } from "components/ReturnButton";
 
 const Summary = () => {
-  const summaryAnswers = useSelector((state) => state.quiz.answers)
-  // const summaryQuestions = useSelector((state) => state.quiz.question)
-  console.log('summaryAnswers', summaryAnswers)
-  // console.log('summaryQuestions', summaryQuestions)
+  const summaryAnswers = useSelector((state) => state.quiz.answers);
+  const result = summaryAnswers.filter((answer) => answer.isCorrect);
 
   return (
-    <div>
-      <p>you answered {summaryAnswers[0].isCorrect}</p>
-      <p>{summaryAnswers[1].answer}</p>
-      {/* <p>{answerIndex.length} </p> */}
+    <>
+      <div>
+        <p>you got {result.length}/{summaryAnswers.length} answers correct!</p>
+      </div>
+      <div>
+        <h1>Your quiz summary</h1>
+
+          {summaryAnswers.map((results) => (
+          <div>
+            <h2>Question number: {results.questionId}</h2>
+            <h3>You answered: {results.answer}</h3>
+
+          {results.isCorrect ? (
+            <h4> Your answer is correct </h4>
+          ) : (
+            <h4> Your answer is not correct </h4>
+          )}
+          {!results.isCorrect && (
+            <p>
+              Correct answer is:{" "}
+              {results.question.options[results.question.correctAnswerIndex]}
+            </p>
+          )}
+        </div>
+      ))}
+      <ReturnButton />
     </div>
-  )
-}
+    </>
+  );
+  };
 
 export default Summary;
-
