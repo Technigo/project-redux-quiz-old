23d12a9e67f56c0bcbc571126bff61cbad1c05f7
miadahlgren <101955676+miadahlgren@users.noreply.github.com>
2022-10-28 11:43:04 +0200

    styled the progress bar and the label above it

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,27 +1,72 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components';
+/* import { popcorn } from 'assets/svg/double-arrow-right-icon.svg' */
 /* import { quiz } from 'reducers/quiz'; */
 
 export const ProgressBar = () => {
   const currentQuestion = useSelector((store) => store.quiz.currentQuestionIndex + 1)
   const finalQuestion = useSelector((store) => store.quiz.questions.length)
-
   return (
-    <div>
-      <p>Question {currentQuestion} / {finalQuestion}</p>
-      <ProgressWrapper>
+    <ProgressWrapper>
+      <section className="progress-status">
+        <label htmlFor="progress">
+          <span>Question: {currentQuestion} out of {finalQuestion}</span>
+        </label>
         <progress
-          className="progress"
           id="progress"
           value={currentQuestion}
           max={finalQuestion} />
-      </ProgressWrapper>
-    </div>
-  )
+      </section>
+    </ProgressWrapper>
+  );
 }
 
-const ProgressWrapper = styled.div`
+const ProgressWrapper = styled.section`
+
+progress {
+  width: 150px;
+  height: 4vh;
+  display: block;
+  appearance: none;
+  border: none;
+  margin-top: 5px;
+}
+
+span{
+color: lightgrey;
+font-size: 18px;
+margin-left: 10px;
+}
+
+progress::-webkit-progress-bar {
+  background: white;
+  border-radius: 50px;
+  padding: 2px;
+}
+
+progress::-moz-progress-bar {
+  background: #fff;
+  border-radius: 50px;
+  padding: 2px;
+}
+
+progress::-webkit-progress-value {
+  background: #d07214;
+  border-radius: 50px;
+  animation: move 1s linear 0 infinite;
+}
+
+progress::-moz-progress-value {
+  background: #D3A13B;
+  border-radius: 50px;
+  animation: move 5s linear 0 infinite;
+}
+
+@keyframes move {
+  0% {background-position: 0px 0px, 0 0, 0 0}
+  100% {background-position: -100px 0px, 0 0, 0 0}
+}
 
 
 
