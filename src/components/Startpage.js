62d3f5290fc0975ebaa5711b6bcff865aeb4d325
carimore <110664183+carimore@users.noreply.github.com>
2022-10-29 09:29:38 +0200

    Update Startpage.js

--- src/components/Startpage.js
+++ src/components/Startpage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Cookie from 'Images/cookie.png'
+import tree from 'Images/tree.png'
 import { Background, QuestionContainer, Headers, Buttons, Illustration } from './Styling/GlobalStyles'
 
 export const StartPage = () => {
@@ -8,11 +8,11 @@ export const StartPage = () => {
     <Background>
       <QuestionContainer>
         <Headers> How much do you know about Christmas traditions from around the world?</Headers>
-        <Illustration src={Cookie} alt="Ginger Cookie" />
         <Link to="/quiz">
           <Buttons href="" className="button"> START QUIZ </Buttons>
         </Link>
       </QuestionContainer>
+      <Illustration src={tree} alt="Ginger Cookie" />
     </Background>
   )
 }
