80ff702a16aef1e23ff0103fc06c4c77f0f0205f
fannystenberg <ffannystenberg@gmail.com>
2023-04-07 17:46:07 +0200

    showing correct  / wrong answer

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { quiz } from 'reducers/quiz';
 
 export const CurrentQuestion = () => {
   const dispatch = useDispatch();
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex]);
+  const answer = useSelector(
+    (state) => state.quiz.answers.find((a) => (a.questionId === question.id))
+  );
 
   const handleAnswer = (questionId, answerIndex) => {
     dispatch(quiz.actions.submitAnswer({ questionId, answerIndex }))
+    if (question.correctAnswerIndex === answerIndex) {
+      document.getElementById(`${answerIndex}`).style.background = 'green'
+    } else {
+      document.getElementById(`${answerIndex}`).style.background = 'red'
+    }
   };
 
   const handleNextQuestion = () => {
@@ -22,15 +31,37 @@ export const CurrentQuestion = () => {
     <section className="quizWrapper">
       <h2>{question.questionText}</h2>
       <div className="answerWrapper">
-        {question.options.map((answer) => {
+        {question.options.map((option, index) => {
           return (
-            <button type="button" className="answer" onClick={handleAnswer}>{answer}</button>
+            <AnswerOptions
+              type="button"
+              className={answer ? 'disabled-true' : 'disabled-false'}
+              disabled={answer}
+              id={index}
+              key={option}
+              onClick={() => handleAnswer(question.id, index)}>
+              {option}
+            </AnswerOptions>
           )
         })}
       </div>
-      <p>Your answer is</p>
       <button type="button" onClick={handleNextQuestion}>Next question</button>
       <p>Question: /6</p>
     </section>
   )
 }
+
+const AnswerOptions = styled.button`
+  padding: 10px;
+  border-radius: 10px;
+  border: none;
+  background-color: rgb(181, 135, 251);
+  opacity: 0.8;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
