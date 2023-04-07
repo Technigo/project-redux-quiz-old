b9934d3d1c1a899eadb57e30023a87643b86572f
fannystenberg <ffannystenberg@gmail.com>
2023-04-07 13:57:00 +0200

    got answers to show and quiz to move forward

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { quiz } from 'reducers/quiz';
 
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const dispatch = useDispatch();
+  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex]);
+
+  const handleNextQuestion = () => {
+    dispatch(quiz.actions.goToNextQuestion())
+  };
 
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h2>Oh no! I could not find the current question!</h2>
   }
 
   return (
     <div>
-      <h1>Question: {question.questionText}</h1>
+      <h2>Question: {question.questionText}</h2>
+      <div className="answerWrapper">
+        {question.options.map((answer) => {
+          return (
+            <button type="button" className="answer">{answer}</button>
+          )
+        })}
+      </div>
+      <button type="button" onClick={handleNextQuestion}>Next question</button>
     </div>
   )
 }
