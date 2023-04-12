a205af6aa5f0aa9f9b9851e9486cfbb728358cc7
Nora Terbocs <nora.terbocs@gmail.com>
2023-04-12 15:17:18 +0200

    Created components for Summary page, added accordion for wrong answers

---
+++ src/components/SummaryPage/AccordionComponent.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import './accordion.css';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel
+} from 'react-accessible-accordion';
+import { AnswerText, ContainerRow } from './SummaryPageStyling';
+
+export const AccordionComponent = () => {
+  const answers = useSelector((store) => store.quiz.answers)
+  const wrongAnswers = answers.filter((answer) => answer.isCorrect === false)
+  console.log(wrongAnswers)
+  const countOfWrongAnswers = wrongAnswers.length
+  console.log(countOfWrongAnswers)
+  return (
+    <Accordion allowZeroExpanded>
+      {wrongAnswers.map(((wrongAnswer) => {
+        return (
+          <AccordionItem key={`wrongAnswer-${wrongAnswer.questionId}`}>
+            <AccordionItemHeading>
+              <AccordionItemButton>{wrongAnswer.question.questionText}</AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <ContainerRow>
+                <AnswerText wrong><span>Your answer: </span>
+                  {wrongAnswer.question.options[wrongAnswer.answerIndex]}
+                </AnswerText>
+                <AnswerText><span>Correct answer: </span>
+                  {wrongAnswer.question.options[wrongAnswer.question.correctAnswerIndex
+                  ]}
+                </AnswerText>
+              </ContainerRow>
+            </AccordionItemPanel>
+          </AccordionItem>
+        )
+      }))}
+    </Accordion>
+
+  )
+}
--- src/components/SummaryPage/SummaryPage.js
+++ src/components/SummaryPage/SummaryPage.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { MovingComponent } from 'react-moving-text'
+import { Container, RestartButton } from './SummaryPageStyling';
+import { AccordionComponent } from './AccordionComponent';
 
-export const SummaryPage = () => {
+export const SummaryPage = (props) => {
+  const answers = useSelector((store) => store.quiz.answers)
+  const wrongAnswers = answers.filter((answer) => answer.isCorrect === false)
+  console.log(wrongAnswers)
+  const countOfWrongAnswers = wrongAnswers.length
+  console.log(countOfWrongAnswers)
+  // const correctAnswers = answers.filter((answer) => answer.isCorrect === true)
   return (
-    <div>Summary</div>
+    <Container>
+      <MovingComponent
+        type="zoomIn"
+        duration="2000ms"
+        delay="0s"
+        direction="normal"
+        timing="ease-out"
+        iteration="1"
+        fillMode="none">
+  Your score is {props.score > 0 ? props.score : 0}
+      </MovingComponent>
+      <h2>time taken</h2>
+      <h2>You had {countOfWrongAnswers} incorrect answers:</h2>
+      <AccordionComponent />
+      <RestartButton>Play again
+      </RestartButton>
+    </Container>
   )
 }
