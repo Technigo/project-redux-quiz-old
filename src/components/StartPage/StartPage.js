b7ea96a19c346fbfdbb099e176f83fd2f7c4da2e
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-04-12 15:40:47 +0200

    Basic container

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { quiz } from 'reducers/quiz'
+import { StartContainer } from './StartPageStyling'
 
 export const StartPage = () => {
   const dispatch = useDispatch()
@@ -8,8 +9,10 @@ export const StartPage = () => {
     dispatch(quiz.actions.startQuiz())
   }
   return (
-    <div>
+    <StartContainer>
+      <h1> Q </h1>
+      <img src="https://images.unsplash.com/photo-1599344941194-5eb5eaaaf73d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="start logo" />
       <button type="button" onClick={startOnClick}>Start</button>
-    </div>
+    </StartContainer>
   )
 }
