380cde8ada76606ba3b37228cf40379fa0ee1d62
GG_Cugs <giorgio.cugusi@gmail.com>
2023-04-12 12:12:56 +0200

    first changhes, welcome page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { quiz } from 'reducers/quiz';
+import { WelcomePage } from 'pages/WelcomePage';
 
 import { CurrentQuestion } from 'components/CurrentQuestion';
 
@@ -14,7 +16,12 @@ const store = configureStore({ reducer });
 export const App = () => {
   return (
     <Provider store={store}>
-      <CurrentQuestion />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/quiz" element={<CurrentQuestion />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './welcomepage.css';
+import { useNavigate } from 'react-router-dom';
+
+export const WelcomePage = () => {
+  const navigate = useNavigate();
+  const goToQuiz = () => {
+    navigate('/quiz');
+  }
+  return (
+    <div className="welcome-background">
+      <div className="welcome-text-container">
+        <h1>Welcome to the React knowledge tester</h1>
+        <p>This is a simple quiz to test the knowledge of React</p>
+      </div>
+      <button type="button" onClick={goToQuiz}>Start the quiz</button>
+      <footer className="linkedin-links">
+        <ul>
+          <li><a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">LinkedIn</a></li>
+          <li><a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">LinkedIn</a></li>
+          <li><a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">LinkedIn</a></li>
+        </ul>
+      </footer>
+    </div>
+  )
+}
