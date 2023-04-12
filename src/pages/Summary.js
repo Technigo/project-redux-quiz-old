0cd31636f224ea42aadc3987ccb029a31a594041
GG_Cugs <giorgio.cugusi@gmail.com>
2023-04-12 12:27:53 +0200

    summary page craeated

---
+++ src/pages/Summary.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SummaryPage = () => {
+  return (
+    <div>
+      <h1>Summary Page</h1>
+      <p>import score, import button to restart, import some celebration</p>
+    </div>
+  )
+}
