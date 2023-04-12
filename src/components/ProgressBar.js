578eb6458b7c7c0df5b9e1adf7b4865aa76d6c69
khleoth <leothunell@gmail.com>
2023-04-12 15:04:03 +0200

    sorted CSS on progress bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Line/* , Circle */ } from 'rc-progress';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+export const ProgressBar = () => {
+  const currentQuestionIndex = useSelector((store) => {
+    return store.quiz.currentQuestionIndex;
+  });
+  const questionTotal = useSelector((store) => store.quiz.questions.length);
+  const percentComplete = (currentQuestionIndex / questionTotal) * 112;
+
+  return (
+    <ProgressMainBarWrapper>
+      <ProgressBarSubWrapper>
+        <Line percent={percentComplete} strokeWidth={1} strokeColor="#3d85c6" style={{ height: '20px', width: '90%' }} />
+        {/* <Circle percent={percentComplete} strokeWidth={1} strokeColor="#D3D3D3" /> */}
+      </ProgressBarSubWrapper>
+    </ProgressMainBarWrapper>
+  )
+};
+
+const ProgressMainBarWrapper = styled.div`
+width: 100vw;
+display: flex;
+justify-content: center;
+margin-top: 50px;
+height: 50px;
+`
+
+const ProgressBarSubWrapper = styled.div`
+width: 80%;
+max-width: 800px;
+height: 100%;
+`
