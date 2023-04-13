f54fffccdd2ce123bf4e42e2e20dc70c17f1abda
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-04-13 14:29:41 +0200

    Add answers and questions map

--- src/components/SummaryTest.js
+++ src/components/SummaryTest.js
@@ -1,16 +1,32 @@
+/* eslint-disable max-len */
 import React from 'react'
 // import styled from 'styled-components';
-// import { useSelector, useDispatch } from "react-redux"
-// import { quiz } from "../reducers/quiz"
+import { useSelector } from 'react-redux'
+// import { quiz } from '../reducers/quiz'
 // import { RestartButton } from './Button'
-import HeroSummaryContainer, { Title } from './SummaryTestStyling';
+import { HeroSummaryContainer, StyledTitle } from './SummaryTestStyling';
 import heroSummaryImage from '../assets/hero-summary-image.jpg';
 
 export const SummaryTest = () => {
+  const answers = useSelector((state) => state.quiz.answers)
   return (
     <HeroSummaryContainer image={heroSummaryImage}>
-      <Title text="Summary" />
-      {/* <RestartButton /> */}
+      <StyledTitle>Summary</StyledTitle>
+      {answers.map((answer) => (
+        <HeroSummaryContainer.AnswerContainer key={answer.question.questionText}>
+          <HeroSummaryContainer.Question>{answer.question.questionText}</HeroSummaryContainer.Question>
+          <HeroSummaryContainer.UserAnswer>
+            {answer.answer}
+            {answer.isCorrect ? '✔️' : '❌'}
+          </HeroSummaryContainer.UserAnswer>
+          <HeroSummaryContainer.CorrectAnswer>
+            {answer.isCorrect ? '' : 'Correct answer: '}
+            {!answer.isCorrect
+              ? answer.question.options[answer.question.correctAnswerIndex]
+              : 'CORRECT'}
+          </HeroSummaryContainer.CorrectAnswer>
+        </HeroSummaryContainer.AnswerContainer>
+      ))}
     </HeroSummaryContainer>
   )
 }
--- src/components/SummaryTestStyling.js
+++ src/components/SummaryTestStyling.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const HeroSummaryContainer = styled.div`
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  object-fit: cover;
+  position: relative;
+  min-height: 100vh;
+  @media screen and (max-width: 768px) {
+    min-height: 100vh;
+  }
+  @media screen and (min-width: 1440px) {
+      min-height: 100vh;
+  }
+  @media screen and (min-width: 1632px) {
+      min-height: 100vh;
+  }
+  @media screen and (max-width: 1024px) {
+      min-height: 100vh;
+  }
+`;
+
 export const StyledTitle = styled.h1`
  font-family: var(--headings-font-family);
  font-size:2rem;
@@ -15,32 +37,38 @@ export const StyledTitle = styled.h1`
   transform: translateX(-50%);
 `
 
-export const Title = ({ text }) => {
+const Title = ({ text }) => {
   return (
     <StyledTitle>{text}</StyledTitle>
   );
 };
 
-const HeroSummaryContainer = styled.div`
-  background-image: url(${(props) => props.image});
-  background-size: cover;
-  background-position: center;
-  display: flex;
-  object-fit: cover;
-  position: relative;
-  min-height: 100vh;
-  @media screen and (max-width: 768px) {
-    min-height: 100vh;
-    background-position: left;
-  }
-  @media screen and (min-width: 1440px) {
-    min-height: 80vh;
-  }
-  @media screen and (min-width: 1632px) {
-    min-height: 90vh;
-  }
-  @media screen and (max-width: 1024px) {
-    min-height: 80vh;
-  }
+const AnswerContainer = styled.div`
+  margin: 20px 0;
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
 `;
-export default HeroSummaryContainer;
+
+const Question = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
+
+const UserAnswer = styled.p`
+  font-size: 1.2rem;
+  margin: 10px 0;
+`;
+
+const CorrectAnswer = styled.p`
+  font-size: 1rem;
+  margin: 10px 0;
+`;
+
+HeroSummaryContainer.AnswerContainer = AnswerContainer;
+HeroSummaryContainer.Question = Question;
+HeroSummaryContainer.UserAnswer = UserAnswer;
+HeroSummaryContainer.CorrectAnswer = CorrectAnswer;
+
+export { HeroSummaryContainer, Title };
