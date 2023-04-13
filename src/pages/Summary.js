fe7aaf32a610e95b655476890ea6cc0d6df77621
malvinamaria <51817058+malvinamaria@users.noreply.github.com>
2023-04-13 18:03:33 +0200

    add text to summery page depending on score (#5)

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,3 +1,5 @@
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable react/jsx-no-useless-fragment */
 import React from 'react';
 import { ResetButton } from 'components/ResetButton';
 import { useSelector } from 'react-redux';
@@ -5,16 +7,32 @@ import { useSelector } from 'react-redux';
 export const SummaryPage = () => {
   // Variable to calculate how many correct answer the user has given
   const correctAnswer = useSelector(
-    (state) => state.quiz.answers.filter((answer) => answer.isCorrect === true).lenght
+    (state) =>
+      state.quiz.answers.filter((answer) => answer.isCorrect === true).length
   );
-  console.log(correctAnswer);
+  const answers = useSelector((state) => state.quiz.answers);
+
+  const userSummary = () => {
+    if (correctAnswer === 5) {
+      return 'Youa are a genius';
+    } else if (correctAnswer === 4) {
+      return 'Well done';
+    } else if (correctAnswer === 3) {
+      return 'Well done';
+    } else {
+      return 'You need to study more';
+    }
+  };
+
+  // console.log(correctAnswer);
   return (
     <div>
       <h1>Summary Page</h1>
-      {` your final score was: ${correctAnswer}
-      import score, import button to restart, import some celebration
-      `}
+      <h3>
+        Your final score was: {correctAnswer} out of {answers.length}
+      </h3>
       <ResetButton />
+      <p>{userSummary()}</p>
     </div>
-  )
-}
+  );
+};
