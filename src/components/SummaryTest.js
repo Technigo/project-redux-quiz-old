5642bf13af47ae547c9369ac0c4da8df0ce39463
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-04-13 15:57:07 +0200

    Add comments

--- src/components/SummaryTest.js
+++ src/components/SummaryTest.js
@@ -7,6 +7,15 @@ import { useSelector } from 'react-redux'
 import { HeroSummaryContainer, StyledTitle } from './SummaryTestStyling';
 import heroSummaryImage from '../assets/hero-summary-image.jpg';
 
+// This code creates a React component that displays a summary of quiz answers.
+// It uses the HeroSummaryContainer component from the SummaryTestStyling.js file to create a container with a hero summary image as a background.
+// Inside the container, the component generates a list of answer summaries using the map method on the answers array.
+// For each answer object in the array, the component creates an AnswerContainer component with a unique key based on the question text.
+// Inside the AnswerContainer, there is a Question component that displays the text of the question.
+// Next, there is a UserAnswer component that shows the user's answer and a checkmark if the answer is correct, or a cross if it is incorrect.
+// Finally, there is a CorrectAnswer component that displays the correct answer if the user's answer is incorrect, or the text "CORRECT" if the user's answer is correct.
+// In summary, this component creates a user-friendly summary of the quiz answers that shows both the user's answer and the correct answer.
+
 export const SummaryTest = () => {
   const answers = useSelector((state) => state.quiz.answers)
   return (
