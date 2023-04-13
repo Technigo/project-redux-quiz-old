4d4d76ea76263515e861f02a7dd6ab3f11bd1c61
malvinamaria <51817058+malvinamaria@users.noreply.github.com>
2023-04-13 18:36:49 +0200

    add css styling to the welcome page (#6)

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -6,21 +6,35 @@ export const WelcomePage = () => {
   const navigate = useNavigate();
   const goToQuiz = () => {
     navigate('/quiz');
-  }
+  };
   return (
     <div className="welcome-background">
       <div className="welcome-text-container">
         <h1>Welcome to the React knowledge tester</h1>
         <p>This is a simple quiz to test the knowledge of React</p>
       </div>
-      <button type="button" onClick={goToQuiz}>Start the quiz</button>
+      <button type="button" onClick={goToQuiz}>
+        Start the quiz
+      </button>
       <footer className="linkedin-links">
         <ul>
-          <li><a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">LinkedIn</a></li>
-          <li><a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">LinkedIn</a></li>
-          <li><a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">LinkedIn</a></li>
+          <li>
+            <a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/joshua-harris-7310b2109/">
+              LinkedIn
+            </a>
+          </li>
         </ul>
       </footer>
     </div>
-  )
-}
+  );
+};
