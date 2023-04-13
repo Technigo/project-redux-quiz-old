4c3422f63c18b7cc409aa8693df2c3868a4b6b4f
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-13 13:45:18 +0200

    design adding to timer

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
 
 export const Timer = () => {
   const [seconds, setSeconds] = useState(0);
@@ -12,6 +13,15 @@ export const Timer = () => {
   }, [seconds]);
 
   return (
-    <p>{seconds}</p>
+    <TimerStyle>TIMER:{seconds}</TimerStyle>
   )
 }
+
+const TimerStyle = styled.div`
+padding: 15px;
+border: none;
+border-radius: 5px;
+font-weight: bold;
+background: transparent;
+font-size: 12px;
+`
