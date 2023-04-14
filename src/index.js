6ba418b720a85aee22da20343430526aca08736b
Fiona Klacar <fiona.klacar@gmail.com>
2023-04-14 15:24:16 +0200

    changed names of folders

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styling/index.css';
 import './styling/header.css'
-import './styling/currentQuestion.css'
-import './styling/progressBar.css';
+import './styling/currentquestionfff.css'
+import './styling/progressbarfff.css';
 import './styling/summary.css'
-import './styling/restartButton.css'
+import './styling/restartbuttonfff.css'
 import './styling/footer.css';
 
 const container = document.getElementById('root');
