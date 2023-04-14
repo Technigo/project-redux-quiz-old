2d0180577c9c34d32094e16653ab3fae190c250d
Fiona Klacar <fiona.klacar@gmail.com>
2023-04-14 15:25:45 +0200

    tidied folders

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styling/index.css';
-import './styling/header.css'
-import './styling/currentquestionfff.css'
-import './styling/progressbarfff.css';
-import './styling/summary.css'
-import './styling/restartbuttonfff.css'
+import './styling/header.css';
+import './styling/currentquestion.css';
+import './styling/progressbar.css';
+import './styling/summary.css';
+import './styling/restartbutton.css';
 import './styling/footer.css';
 
 const container = document.getElementById('root');
