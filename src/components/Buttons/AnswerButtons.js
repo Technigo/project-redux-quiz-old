97bcdc217764c28c1f3e980aecef3cf4f467bf12
khleoth <leothunell@gmail.com>
2023-04-14 12:00:08 +0200

    sorted colorchange on answerclick

--- src/components/Buttons/AnswerButtons.js
+++ src/components/Buttons/AnswerButtons.js
@@ -1,35 +1,27 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable no-nested-ternary */
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { quiz } from 'reducers/quiz'
+import { useDispatch, useSelector } from 'react-redux';
+import { quiz } from 'reducers/quiz';
 import styled from 'styled-components';
 
 export const AnswerButton = () => {
+  const [selectedOption, setSelectedOption] = useState();
   const [showSelectedColor, setShowSelectedColor] = useState(false);
-  const [AnswerColor, setAnswerColor] = useState('green')
-  const [selectedIndex, setSelectedIndex] = useState();
   const dispatch = useDispatch();
-  const question = useSelector((store) => store.quiz.questions[store.quiz.currentQuestionIndex]);
-  /* const rightAnswer = useSelector((store) => store.quiz.questions.correctAnswerIndex); */
-  const [guessedAnswerIndex, setGuessedAnswerIndex] = useState('');
-  const onButtonClick = (index) => {
-    dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))
-    setSelectedIndex(index);
+  const { currentQuestionIndex, questions } = useSelector((store) => store.quiz);
+  const question = questions[currentQuestionIndex];
+
+  const handleOptionClick = (index) => {
+    setSelectedOption(index);
     setShowSelectedColor(true);
-    if (index === question.correctAnswerIndex) {
-      setAnswerColor('green');
-    } else {
-      setAnswerColor('red');
-    }
     setTimeout(() => {
-      setSelectedIndex(100);
-      setAnswerColor('pink');
+      dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }));
       setShowSelectedColor(false);
+      setSelectedOption(undefined);
       dispatch(quiz.actions.goToNextQuestion());
     }, 1000);
-  }
+  };
 
-  console.log(guessedAnswerIndex)
   return (
     <BtnsWrapper>
       {question.options.map((singleOption, index) => (
@@ -37,44 +29,39 @@ export const AnswerButton = () => {
           key={singleOption}
           type="button"
           value={index}
-          AnswerColor={AnswerColor}
-          selectedIndex={selectedIndex}
-          aria-label={showSelectedColor && index === guessedAnswerIndex ? AnswerColor : ''}
-          onClick={(e) => {
-            setGuessedAnswerIndex(Number(e.target.value));
-            onButtonClick(index);
-          }}>
+          showSelectedColor={showSelectedColor && selectedOption === index}
+          index={index}
+          isCorrectAnswer={index === question.correctAnswerIndex}
+          aria-label={`Answer option ${index + 1}: ${singleOption}`}
+          onClick={() => handleOptionClick(index)}>
           {singleOption}
         </Btn>
       ))}
     </BtnsWrapper>
-  )
-}
+  );
+};
+
 const Btn = styled.button`
   border-style: double;
-  border-color: ${(props) => ((props.showSelectedColor && props.selectedIndex === props.index) ? props.AnswerColor : 'none')};
+  border-color: black;
   padding: 10px;
-  color: black;
+  color: ${(props) => (props.showSelectedColor ? (props.isCorrectAnswer ? '#4CAF50' : '#da1717') : '#fff')};
   border-radius: 10px;
   margin-top: 6px;
   font-size: 16px;
   font-weight: 600;
-  border: solid 2px black; 
+  border: solid 2px black;
   width: 133px;
   background-color: #3d85c6;
 
   &:hover {
-    background-color: black;
-    color: #3d85c6;
-    cursor: pointer;
     transform: scale(1.2);
-    border-color:#3d85c6;
   }
 `;
 
 const BtnsWrapper = styled.div`
-display:flex;
-flex-wrap: wrap;
-justify-content: space-around;
-width: 100%;
-`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 100%;
+`;
