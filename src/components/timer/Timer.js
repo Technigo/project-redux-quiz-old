23ecdbe25a696e5827ecec32c6dc83c983bbaa8b
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-15 10:03:04 +0200

    set timer to count down

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,44 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components'
-import IconImg from 'assets/hourglass.svg'
-import Rotate from './TimerAni'
+import styled from 'styled-components';
+import IconImg from 'assets/hourglass.svg';
+import Rotate from './TimerAni';
 
 export const Timer = () => {
-  const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(300);
+  const [isTimeUp, setIsTimeUp] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setSeconds((prevSeconds) => prevSeconds + 1);
+      setSeconds((prevSeconds) => prevSeconds - 1);
     }, 1000);
 
+    // Check if time is up
+    if (seconds === 0) {
+      setIsTimeUp(true);
+      clearInterval(interval);
+    }
+
     return () => clearInterval(interval);
-  }, []);
+  }, [seconds]);
 
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
 
   return (
     <TimerStyle>
-      <Rotate> <Icon src={IconImg} alt="timer-icon" /></Rotate>
-      {minutes < 10 ? `0${minutes}` : minutes}:
-      {remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds}
+      <Rotate>
+        <Icon src={IconImg} alt="timer-icon" />
+      </Rotate>
+      {isTimeUp ? ( // Display "Aw, too slow! Try again" when time is up
+        <div>Aw, too slow! Try again</div>
+      ) : (
+        // Display remaining time in minutes and seconds
+        <>
+          {minutes < 10 ? `0${minutes}` : minutes}:
+          {remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds}
+        </>
+      )}
     </TimerStyle>
   );
 };
 
 const TimerStyle = styled.div`
-    display: flex;
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
-    font-weight: bold;
-    background: transparent;
-    font-size: 16px;
-    gap: 10px;
-    background-color: rgb(61, 133, 198);
-`
+  display: flex;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-weight: bold;
+  background: transparent;
+  font-size: 16px;
+  gap: 10px;
+  background-color: rgb(61, 133, 198);
+`;
+
 export const Icon = styled.img`
-width: 20px;
-height: 20px;
-margin-top: 30px;
-`
+  width: 20px;
+  height: 20px;
+  margin-top: 30px;
+`;
