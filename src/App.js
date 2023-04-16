102d4fba18fc3c631db01dc9627d523cbf7bcd86
SandraMadeleine <sandramohlsson@yahoo.com>
2023-04-16 16:26:30 +0200

    prep for merge

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { quiz } from 'reducers/quiz';
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import { quiz } from 'reducers/quiz';
 
-import Start from 'components/Start';
-// import { CurrentQuestion } from 'components/CurrentQuestion';
+// import Start from 'components/Start';
+// // import { CurrentQuestion } from 'components/CurrentQuestion';
 
-const reducer = combineReducers({
-  quiz: quiz.reducer
-});
+// const reducer = combineReducers({
+//   quiz: quiz.reducer
+// });
 
-const store = configureStore({ reducer });
+// const store = configureStore({ reducer });
 
-export const App = () => {
-  return (
-    <Provider store={store}>
-      <Start />
-      {/* <CurrentQuestion /> */}
-    </Provider>
-  );
-}
+// export const App = () => {
+//   return (
+//     <Provider store={store}>
+//       <Start />
+//       {/* <CurrentQuestion /> */}
+//     </Provider>
+//   );
+// }
--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,45 +1,45 @@
-/* eslint-disable implicit-arrow-linebreak */
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { Button, AnswerButton, NextQuestion } from './Button'
-import { SummaryTest } from './SummaryTest';
+// /* eslint-disable implicit-arrow-linebreak */
+// import React, { useState } from 'react'
+// import { useSelector } from 'react-redux'
+// import { Button, AnswerButton, NextQuestion } from './Button'
+// import { SummaryTest } from './SummaryTest';
 
-export const CurrentQuestion = () => {
-  const [disabled, setDisabled] = useState(false);
+// export const CurrentQuestion = () => {
+//   const [disabled, setDisabled] = useState(false);
 
-  const question = useSelector(
-    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
-  );
-  const quizOver = useSelector((state) => state.quiz.quizOver);
-  const userAnswer = useSelector((state) =>
-    state.quiz.answers.find((a) => a.questionId === question.id));
-  if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
-  }
-  if (quizOver) {
-    return (
-      <SummaryTest />
-    )
-  }
+//   const question = useSelector(
+//     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+//   );
+//   const quizOver = useSelector((state) => state.quiz.quizOver);
+//   const userAnswer = useSelector((state) =>
+//     state.quiz.answers.find((a) => a.questionId === question.id));
+//   if (!question) {
+//     return <h1>Oh no! I could not find the current question!</h1>
+//   }
+//   if (quizOver) {
+//     return (
+//       <SummaryTest />
+//     )
+//   }
 
-  return (
-    <div>
-      <h1>Question: {question.questionText}</h1>
-      <div className="options">
-        {question.options.map((item, index) => (
-          <Button
-            key={item}
-            item={item}
-            index={index}
-            questionId={question.id}
-            question={question}
-            answer={userAnswer}
-            setDisabled={setDisabled}
-            disabled={disabled} />
-        ))}
-        <AnswerButton />
-        <NextQuestion />
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>Question: {question.questionText}</h1>
+//       <div className="options">
+//         {question.options.map((item, index) => (
+//           <Button
+//             key={item}
+//             item={item}
+//             index={index}
+//             questionId={question.id}
+//             question={question}
+//             answer={userAnswer}
+//             setDisabled={setDisabled}
+//             disabled={disabled} />
+//         ))}
+//         <AnswerButton />
+//         <NextQuestion />
+//       </div>
+//     </div>
+//   )
+// }
