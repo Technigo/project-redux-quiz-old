14e9cb5a776cab0001041957a7b77049fe953083
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-28 13:37:05 +0200

    styling starterpage

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -6,41 +6,44 @@ export const StartPage = (() => {
   return (
     <div>
       <Link to="/quiz">
-        <button type="button">
-          <h1>
+        <button type="button" className="StartButton">
+          <div className="Text">
+            <h1>
 What heter djuret?
-          </h1>
+            </h1>
 
-          <h2>
+            <h2>
 ¿Como se llama el animal?
-          </h2>
-          <p>Börja spela empieza a jugar</p>
+            </h2>
+            <p>Börja spela empieza a jugar</p>
+          </div>
+          <div className="animations">
+            <Player
+              src="https://assets6.lottiefiles.com/packages/lf20_lb6Gsk.json"
+              className="player"
+              loop
+              autoplay
+              speed={1}
+              style={{
+                width: '100%',
+                height: '10rem',
+                top: 0,
+                left: 0
+              }} />
 
-          <Player
-            src="https://assets6.lottiefiles.com/packages/lf20_lb6Gsk.json"
-            className="player"
-            loop
-            autoplay
-            speed={1}
-            style={{
-              width: '100%',
-              height: '10rem',
-              top: 0,
-              left: 0
-            }} />
-
-          <Player
-            src="https://assets6.lottiefiles.com/packages/lf20_lc46h4dr.json"
-            className="player"
-            loop
-            autoplay
-            speed={1}
-            style={{
-              width: '100%',
-              height: '10rem',
-              top: 0,
-              left: 0
-            }} />
+            <Player
+              src="https://assets6.lottiefiles.com/packages/lf20_lc46h4dr.json"
+              className="player"
+              loop
+              autoplay
+              speed={1}
+              style={{
+                width: '100%',
+                height: '10rem',
+                top: 0,
+                left: 0
+              }} />
+          </div>
         </button>
       </Link>
 
